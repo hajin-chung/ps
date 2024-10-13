e9de61acfec6c41609c2f41893fa61da89c99dfa
hajin-chung <hajinchung1@gmail.com>
2024-10-13 05:41:16 +0000

    boj 1761

--- boj/1761.cpp
+++ boj/1761.cpp
@@ -3,24 +3,50 @@ using namespace std;
 
 typedef pair<int, int> pii;
 
-int n, m, md;
+int n, m, md, ml;
 vector<vector<pii>> a, dp;
-vector<int> depth;
+vector<int> depth, isChild;
 
 void dfs(int curr) {
-  int max_level = (int)floor(log2(depth[curr]));
-  dp.resize(max_level);
-
-  for (int i = 1; i < max_level; i++) {
-    int p = dp[curr][i-1].first;
-    dp[curr][i] = {dp[p][i-1].first, dp[curr][i-1].second + dp[p][i-1].second};
+  md = max(md, depth[curr]);
+  if (depth[curr] > 0) {
+    int max_level = (int)floor(log2(depth[curr]));
+    for (int i = 1; i <= max_level; i++) {
+      int p = dp[curr][i-1].first;
+      dp[curr][i] = {dp[p][i-1].first, dp[curr][i-1].second + dp[p][i-1].second};
+    }
   }
 
-  for (auto {next, w} : a[curr]) {
+  for (auto [next, w] : a[curr]) {
     depth[next] = depth[curr] + 1;
+
+    int max_level = (int)floor(log2(depth[next]));
+    dp[next].resize(max_level + 1);
     dp[next][0] = {curr, w};
     dfs(next);
   }
+}
+
+int lca(int x, int y) {
+  int ret = 0;
+  if (depth[x] != depth[y]) {
+    if (depth[x] > depth[y]) swap(x, y);
+    for (int i = dp[y].size()-1; i >= 0; i--)
+      if (depth[x] <= depth[dp[y][i].first]) {
+        ret += dp[y][i].second;
+        y = dp[y][i].first;
+      }
+  }
+  if (x != y) {
+    for (int i = dp[x].size()-1; i >= 0; i--) {
+      if (dp[x][i] != dp[y][i]) {
+        x = dp[x][i].first;
+        y = dp[y][i].first;
+      }
+      ret += dp[x][i].second + dp[y][i].second;
+    }
+  }
+  return ret;
 }
 
 int main() {
@@ -30,20 +56,33 @@ int main() {
 
   int i;
   int x, y, z;
+  int root;
 
   cin >> n;
-  md = (int)floor(log2(n));
   dp.resize(n+1);
   depth.resize(n+1);
   a.resize(n+1);
-  for (i = 0; i < n; i++) {
+  isChild.resize(n+1, 0);
+  for (i = 0; i < n-1; i++) {
     cin >> x >> y >> z; 
+    isChild[y] = 1;
     a[x].push_back({y, z});
   }
 
-  depth[1] = 1;
-  dfs(1);
-  for (int i = 1; i <= n; i++) {
+  for (i = 1; i <= n; i++) if (!isChild[i]) root = i;
 
+  dfs(root);
+  // for (int i = 1; i <= n; i++) {
+  //   cout << i << ": ";
+  //   for (auto [p, w] : dp[i])
+  //     cout << "{" << p << ", " << w << "}, ";
+  //   cout << "\n";
+  // }
+  // cout << endl;
+
+  cin >> m;
+  while (m--) {
+    cin >> x >> y;
+    cout << lca(x, y) << "\n";
   }
 }
