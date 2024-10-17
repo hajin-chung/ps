5e20ce2f1c1921ac3925576d969c338c111bf16a
hajin-chung <hajinchung1@gmail.com>
2024-10-17 10:22:05 +0000

    sync

--- boj/1533.cpp
+++ boj/1533.cpp
@@ -6,13 +6,12 @@ typedef long long int ll;
 typedef vector<vector<ll>> mat;
 
 int n, s, e, t;
-mat a;
-mat ans;
+mat a, ans;
 mat operator *(mat &a, mat &b) {
-  mat res(5*n, vector<ll>(5*n));
-  for (int i = 0; i < 5*n; i++)
-    for (int j = 0; j < 5*n; j++)
-      for (int k = 0; k < 5*n; k++) {
+  mat res(5*n+1, vector<ll>(5*n+1));
+  for (int i = 1; i <= 5*n; i++)
+    for (int j = 1; j <= 5*n; j++)
+      for (int k = 1; k <= 5*n; k++) {
         res[i][k] += a[i][j] * b[j][k];
         res[i][k] %= MOD;
       }
@@ -21,7 +20,7 @@ mat operator *(mat &a, mat &b) {
 
 void matpow() {
   while (t) {
-    if (t%2) ans *= a;
+    if (t%2) ans = ans * a;
     t /= 2;
     a = a * a;
   }
@@ -29,19 +28,20 @@ void matpow() {
 
 int main() {
   scanf("%d%d%d%d", &n, &s, &e, &t);
-  a.resize(5*n, vector<ll>(5*n));
-  ans.resize(5*n, vector<ll>(5*n));
-  for (int i = 0; i < n; i++) {
+  a.resize(5*n+1, vector<ll>(5*n+1));
+  ans.resize(5*n+1, vector<ll>(5*n+1));
+  for (int i = 1; i <= n; i++) {
     char tmp[12];
     scanf("%s", tmp);
-    for (int j = 0; j < n; j++) {
-      ll w = tmp[j] - '0';
+    for (int j = 1; j <= n; j++) {
+      ll w = tmp[j-1] - '0';
       if (1 <= w && w <= 5)
         a[5*i][5*j - w + 1] = 1;
     }
-    for (int j = 0; j < 4; j++)
-      a[5*i - j][5*i - j+1]= 1;
+    for (int j = 1; j <= 4; j++)
+      a[5*(i-1) + j][5*(i-1) + j + 1]= 1;
   }
+  for (int i = 1; i <= 5*n; i++) ans[i][i] = 1;
   matpow();
-  printf("%lld\n", ans[5*(s-1)][5*(e-1)]);
+  printf("%lld\n", ans[5*s][5*e]);
 }
