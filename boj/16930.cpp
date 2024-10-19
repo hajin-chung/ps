a2ba5e579929092c30f086c51b4db216603b1d37
hajin-chung <hajinchung1@gmail.com>
2024-10-19 08:40:14 +0000

    sync

--- boj/16930.cpp
+++ boj/16930.cpp
@@ -5,14 +5,13 @@
 using namespace std;
 
 typedef pair<int, int> pii;
-typedef pair<int, pii> pip;
 int dy[4] = {0, 1, 0, -1}, dx[4] = {1, 0, -1, 0};
 
 int main() {
   int n, m, k;
   char a[1005][1005];
   int d[1005][1005];
-  queue<pip> Q;
+  queue<pii> Q;
   pii s, e;
   
   scanf("%d %d %d", &n, &m, &k);
@@ -23,22 +22,23 @@ int main() {
     for (int j = 0; j < m; j++)
       d[i][j] = INF;
 
-  Q.push({0, s});
+  Q.push(s);
   d[s.fi][s.se] = 0;
   while (!Q.empty()) {
-    pip t = Q.front(); Q.pop();
-    int yy = t.se.fi, xx = t.se.se;
-    if (d[yy][xx] < t.fi) continue;
-    if (e.fi == yy && e.se == xx) break;
+    auto [yy, xx] = Q.front(); Q.pop();
     for (int i = 0; i < 4; i++)
       for (int j = 1; j <= k; j++) {
         int ty = dy[i]*j + yy;
         int tx = dx[i]*j + xx;
         if (ty < 0 || ty >= n || tx < 0 || tx >= m) continue;
         if (a[ty][tx] == '#') break;
-        if (d[ty][tx] < d[yy][xx] + 1) break;
+        if (d[ty][tx] <= d[yy][xx]) break;
         d[ty][tx] = d[yy][xx] + 1;
-        Q.push({d[ty][tx], {ty, tx}});
+        if (ty == e.fi && tx == e.se) {
+          printf("%d\n", d[ty][tx]);
+          return 0;
+        }
+        Q.push({ty, tx});
       }
   }
   if (d[e.fi][e.se] == INF) printf("-1\n");
