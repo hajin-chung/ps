658506e2eb9052b9111ab455cd7664c7479966ec
hajin-chung <hajinchung1@gmail.com>
2024-10-19 10:13:44 +0000

    sync

--- boj/3015.cpp
+++ boj/3015.cpp
@@ -1,4 +1,6 @@
 #include <bits/stdc++.h>
+#define fi first
+#define se second
 using namespace std;
 
 typedef long long int ll;
@@ -6,14 +8,23 @@ typedef long long int ll;
 int main() {
   int n, x;
   ll cnt = 0;
-  stack<int> S;
+  stack<pair<int, int>> S;
 
   scanf("%d", &n);
   while (n--) {
     scanf("%d", &x);
-    cnt += (ll)S.size();
-    while (!S.empty() && S.top() < x) S.pop();
-    S.push(x);
+    while (!S.empty() && S.top().fi < x) {
+      cnt += S.top().se;
+      S.pop();
+    }
+    int v = 1;
+    if (!S.empty() && S.top().fi == x) {
+      v += S.top().se;
+      cnt += S.top().se;
+      S.pop();
+    }
+    if (!S.empty()) cnt++;
+    S.push({x, v});
   }
   printf("%lld\n", cnt);
 }
