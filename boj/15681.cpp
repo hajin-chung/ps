d4b9f950be1341eb313738b731403dfda4c4bbfe
hajin-chung <hajinchung1@gmail.com>
2024-10-19 08:08:10 +0000

    sync

---
+++ boj/15681.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<vector<int>> adj;
+vector<int> cnt, chk;
+
+int dfs(int curr) {
+  chk[curr] = 1;
+  cnt[curr] = 1;
+  if (adj[curr].size() == 1) return cnt[curr];
+  for (auto next : adj[curr])
+    if (!chk[next]) 
+      cnt[curr] += dfs(next);
+  return cnt[curr];
+}
+
+int main() {
+  int n, r, q;
+
+  scanf("%d %d %d", &n, &r, &q);
+  adj.resize(n+1);
+  cnt.resize(n+1);
+  chk.resize(n+1);
+  for (int i = 0; i < n-1; i++) {
+    int u, v;
+    scanf("%d %d", &u, &v);
+    adj[u].push_back(v);
+    adj[v].push_back(u);
+  }
+  dfs(r);
+  for (int i = 0; i < q; i++) {
+    int u;
+    scanf("%d", &u);
+    printf("%d\n", cnt[u]);
+  }
+}
