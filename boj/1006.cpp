9e8715d77526c52247ad83ad38c60717d44670d9
hajin-chung <hajinchung1@gmail.com>
2024-10-20 02:05:56 +0000

    sync

---
+++ boj/1006.cpp
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <string.h>
+#define INF 1000000000
+
+int dp[10005][3], a[10005][2], n, w;
+int min2(int a, int b) { return a > b ? b : a; }
+int min(int a, int b, int c) { return min2(a, min2(b, c)); }
+int tps(int a, int b) { return a + b <= w ? 1 : 2; }
+
+void fill() {
+  for (int i = 2; i <= n; i++) {
+    dp[i][0] = min(dp[i-1][0], dp[i-1][1]+1, dp[i-1][2]+1) + tps(a[i][0], a[i][1]);
+    dp[i][1] = min(dp[i-1][2] + tps(a[i-1][0], a[i][0]), dp[i-1][0] + 1, dp[i-1][1] + 2);
+    dp[i][2] = min(dp[i-1][1] + tps(a[i-1][1], a[i][1]), dp[i-1][0] + 1, dp[i-1][2] + 2);
+  }
+}
+
+void print() {
+  printf("\n");
+  for (int i = 0; i < 3; i++) {
+    for (int j = 1; j <= n; j++) {
+      if (dp[j][i] == INF) printf(". ");
+      else printf("%d ", dp[j][i]);
+    }
+    printf("\n");
+  }
+  printf("\n");
+}
+
+void solve() {
+  int ans = INF;
+  scanf("%d%d", &n, &w);
+  for (int i = 0; i < 2; i++)
+    for (int j = 1; j <= n; j++)
+      scanf("%d", &a[j][i]);
+
+  memset(dp, 0, 10005*3*sizeof(int));
+  dp[1][0] = tps(a[1][0], a[1][1]);
+  dp[1][1] = INF;
+  dp[1][2] = INF;
+  fill();
+  ans = min(dp[n][0], dp[n][1]+1, dp[n][2]+1);
+
+  memset(dp, 0, 10005*3*sizeof(int));
+  dp[1][0] = INF;
+  dp[1][1] = tps(a[1][0], a[n][0]);
+  dp[1][2] = INF;
+  fill();
+  ans = min(dp[n][0], dp[n][1]+1, dp[n][2]);
+
+  memset(dp, 0, 10005*3*sizeof(int));
+  dp[1][0] = INF;
+  dp[1][1] = INF;
+  dp[1][2] = tps(a[1][1], a[n][1]);
+  fill();
+  ans = min(dp[n][0], dp[n][1], dp[n][2]+1);
+
+  printf("%d\n", ans);
+}
+
+int main() {
+  int T;
+  scanf("%d", &T);
+  while (T--) solve();
+}
