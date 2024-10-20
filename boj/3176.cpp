4648a870ca08b09a24e6f6039d72749db89f2c7a
hajin-chung <hajinchung1@gmail.com>
2024-10-20 01:21:45 +0000

    sync

--- boj/3176.cpp
+++ boj/3176.cpp
@@ -1,12 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-typedef tuple<int, int, int> t3i;
 typedef pair<int, int> pii;
 
-vector<pii> adj;
-t3i dp[100005][30];
+struct Node {
+  int p, mx, mn;
+};
+
+vector<vector<pii>> adj;
+Node dp[100005][30];
 int chk[100005], depth[100005];
+int n;
 
 void dfs(int curr, int lev) {
   chk[curr] = 1; 
@@ -14,14 +18,64 @@ void dfs(int curr, int lev) {
   for (auto [next, w] : adj[curr])
     if (!chk[next]) {
       dp[next][0] = {curr, w, w};
-      make(next, lev+1);
+      dfs(next, lev+1);
     }
 }
 
-int lca(int a, int b) {
-  
+void lca(int a, int b) {
+  int mn, mx;
+  if (depth[a] < depth[b]) swap(a, b); 
+  mn = mx = dp[a][0].mn;
+
+  if (depth[a] != depth[b]) {
+    for (int i = 29; i >= 0 ; i--)
+      if (depth[dp[a][i].p] > depth[b]) {
+        mn = min(mn, dp[a][i].mn);
+        mx = max(mx, dp[a][i].mx);
+        a = dp[a][i].p;
+      } 
+    mn = min(mn, dp[a][0].mn);
+    mx = max(mx, dp[a][0].mx);
+    a = dp[a][0].p;
+  }
+
+  if (a != b) {
+    for (int i = 29; i >= 0 && a != b; i--)
+      if (dp[a][i].p != dp[b][i].p) {
+        mn = min(mn, min(dp[a][i].mn, dp[b][i].mn));
+        mx = max(mx, max(dp[a][i].mx, dp[b][i].mx));
+        a = dp[a][i].p;
+        b = dp[b][i].p;
+      }
+    mn = min(mn, min(dp[a][0].mn, dp[b][0].mn));
+    mx = max(mx, max(dp[a][0].mx, dp[b][0].mx));
+  }
+  printf("%d %d\n", mn, mx);
 }
 
-void query() {
+int main() {
+  scanf("%d", &n);
+  adj.resize(n+1);
+  for (int i = 0; i < n-1; i++) {
+    int u, v, w;
+    scanf("%d%d%d", &u, &v, &w);
+    adj[u].push_back({v, w});
+    adj[v].push_back({u, w});
+  }
 
+  dfs(1, 0);
+  for (int i = 1; i < 30; i++)
+    for (int j = 1; j <= n; j++) {
+      dp[j][i].p = dp[dp[j][i-1].p][i-1].p;
+      dp[j][i].mn = min(dp[j][i-1].mn, dp[dp[j][i-1].p][i-1].mn);
+      dp[j][i].mx = max(dp[j][i-1].mx, dp[dp[j][i-1].p][i-1].mx);
+    }
+
+  int m;
+  scanf("%d", &m);
+  while (m--) {
+    int u, v;
+    scanf("%d%d", &u, &v);
+    lca(u, v);
+  }
 }
