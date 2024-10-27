ed4eded0c9cbfdf095907f3ea45c9cb77c41e4cc
hajin-chung <hajinchung1@gmail.com>
2024-10-27 13:52:52 +0900

    sync

--- boj/8540.cpp
+++ boj/8540.cpp
@@ -1,40 +1,50 @@
 #include <bits/stdc++.h>
+#include <functional>
+#include <vector>
 #define INF 1000000000
 using namespace std;
 
+typedef pair<int, int> pii;
 vector<vector<int>> adj;
 vector<int> order, lookup;
-int a, n, b, k, m;
+int a, n, b, k, m, e, s;
 int ans = INF;
 
-void dfs(int curr, int cnt, int idx, vector<bool>& chk) {
-  if (curr > a+n && idx == k+1) {
-    ans = min(ans, cnt);
-    return;
-  }
+int isen(int v) {
+  return (a < v && v <= a+n) ? 1 : 0;
+}
 
-  for (auto next : adj[curr]) {
-    if (a < next && next <= a + n) {
-      if (lookup[next] > idx) continue;
-      if (chk[next]) continue;
-      if (lookup[next] == idx) {
-        vector<bool> chk2(a+b+n+1, false);
-        dfs(next, cnt+1, idx+1, chk2);
-      } else {
-        chk[next] = true;
-        dfs(next, cnt+1, idx, chk);
-        chk[next] = false;
+// curr -> curr + 1 withouth touching order[i] where i > idx + 1
+int dij(int idx) {
+  vector<int> d(e+1, INF);
+  priority_queue<pii, vector<pii>, greater<pii>> pq;
+
+  d[order[idx]] = 0;
+  pq.push({0, order[idx]});
+  while (!pq.empty()) {
+    auto [dist, curr] = pq.top(); pq.pop();
+    if (dist > d[curr]) continue;
+    for (auto next : adj[curr]) {
+      if (lookup[next] <= curr+1 && d[next] > d[curr] + isen(next)) {
+        d[next] = d[curr] + isen(next);
+        pq.push({next, d[next]});
       }
-    } else if (a + n < next) {
-      if (idx == k+1) dfs(next, cnt, idx, chk);
     }
   }
+  printf("%d\n", idx);
+  for (auto dist : d) {
+    if (dist == INF) printf(". ");
+    else printf("%d ", dist);
+  }
+  printf("\n");
+  return d[order[idx+1]];
 }
 
 int main() {
   scanf("%d%d%d%d%d",&a,&n,&b,&k,&m);
-  adj.resize(a+n+b+1);
-  lookup.resize(a+n+b+1);
+  s = 0, e = a+n+b+1;
+  adj.resize(e+1);
+  lookup.resize(e+1);
   order.push_back(0);
   for (int i=0;i<k;i++) {
     int x;
@@ -42,6 +52,7 @@ int main() {
     order.push_back(x);
     lookup[x] = i+1;
   }
+  order.push_back(a+n+b+1);
   for (int i=0;i<m;i++) {
     int u, v;
     scanf("%d%d",&u,&v);
@@ -49,10 +60,34 @@ int main() {
     if (u <= a+n && v > a) adj[u].push_back(v);
     if (v <= a+n && u > a) adj[v].push_back(u);
   }
+  for (int i = 1; i <= a; i++) adj[s].push_back(i);
+  for (int i = a+n+1; i <= a+n+b; i++) adj[i].push_back(e);
 
-  for (int i = 1; i <= a; i++) {
-    vector<bool> chk(a+b+n+1, false);
-    dfs(i, 0, 1, chk);
+  for (int i = 0; i <= e; i++) {
+    printf("%d: ", i);
+    for (auto next : adj[i]) printf("%d ", next);
+    printf("\n");
   }
+
+  printf("order: ");
+  for (auto o : order) printf("%d ", o);
+  printf("\n");
+
+  printf("lookup: ");
+  for (auto o : lookup) printf("%d ", o);
+  printf("\n");
+
+  int ans = 0;
+  for (int i = 0; i <= k; i++) {
+    int dist = dij(order[i]);
+    printf("%d %d: %d\n", order[i], order[i+1], dist);
+    if (dist >= INF) {
+      ans = INF;
+      break;
+    } else {
+      ans += dist;
+    }
+  }
+
   printf("%d\n", ans >= INF ? -1 : ans);
 }
