be7701dce1d826b81df48ccc29798ab6190163cb
hajin-chung <hajinchung1@gmail.com>
2024-10-27 11:19:57 +0900

    sync

--- boj/1701.cpp
+++ boj/1701.cpp
@@ -1,18 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-char str[5000];
-int pi[5000], n;
+string str;
 
-int main() {
-  scanf("%s", str); 
-  n = strlen(str);
-  int mx = 0;
+int solve(string pat) {
+  int pi[5000], n, ret = 0;
+  n = pat.size();
+  memset(pi, 0, sizeof(pi));
   int j = 0;
   for (int i = 1; i < n; i++) {
-    while (j > 0 && str[i] != str[j]) j = pi[j-1];
-    if (str[i] == str[j]) pi[i] = ++j;
-    mx = max(mx, pi[i]);
+    while (j > 0 && pat[i] != pat[j]) j = pi[j-1];
+    if (pat[i] == pat[j]) pi[i] = ++j;
+    ret = max(ret, pi[i]);
+  }
+  return ret;
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  int mx = 0;
+  cin >> str;
+  string pat = "";
+  for (int i = 0; i < str.size(); i++) {
+    pat += str[i];
+    mx = max(mx, solve(pat));
   }
   printf("%d\n", mx);
 }
