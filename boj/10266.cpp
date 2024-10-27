cf0e93ecfc640d03e8537e952d654613f7aeb28c
hajin-chung <hajinchung1@gmail.com>
2024-10-27 14:35:30 +0900

    sync

---
+++ boj/10266.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n;
+vector<int> a, b, u, v, pi;
+
+int main() {
+  scanf("%d", &n);
+  int x;
+  for (int i = 0; i < n; i++) {
+    scanf("%d", &x);
+    u.push_back(x);
+  }
+  for (int i = 0; i < n; i++) {
+    scanf("%d", &x);
+    v.push_back(x);
+  }
+  sort(u.begin(), u.end());
+  sort(v.begin(), v.end());
+
+  for (int i = 0; i < n; i++) a.push_back(i ? u[i] - u[i-1] : u[i]+360000-u[n-1]);
+  for (int i = 0; i < n; i++) b.push_back(i ? v[i] - v[i-1] : v[i]+360000-v[n-1]);
+  for (int i = 0; i < n; i++) a.push_back(a[i]);
+
+  int j = 0;
+  pi.resize(n);
+  for (int i = 1; i < n; i++) {
+    while (j > 0 && b[i] != b[j]) j = pi[j-1];
+    if (b[i] == b[j]) pi[j] = ++j;
+  }
+
+  j = 0;
+  for (int i = 1; i < 2*n; i++) {
+    while (j > 0 && b[j] != a[i]) j =pi[j-1];
+    if (b[j] == a[i]) {
+      if (j == n-1) {
+        printf("possible\n");
+        return 0;
+      } else {
+        j++;
+      }
+    }
+  }
+  printf("impossible\n");
+}
