919ebf647e7c7b573ef8bde1d8ac43c1f6924a95
hajin-chung <hajinchung1@gmail.com>
2024-10-28 19:37:45 +0900

    sync

--- boj/3878.cpp
+++ boj/3878.cpp
@@ -105,15 +105,13 @@ bool solve() {
 
   if (n == 0 || m == 0) return true;
   if (n == 1 && m == 1) return true;
-  if (n == 2 && m == 1) return line_point_chk(a[0], a[1], b[0]);
   if (n == 1 && m == 2) return line_point_chk(b[0], b[1], a[0]);
+  if (n == 2 && m == 1) return line_point_chk(a[0], a[1], b[0]);
   if (n == 2 && m == 2) return line_line_chk(a[0], a[1], b[0], b[1]);
-  bool flag = true;
-  if (n >= 3) flag = hull_hull_chk(a, b);
-  if (n >= 3 && m == 2) flag &= hull_line_chk(a, b);
-  if (m >= 3) flag &= hull_hull_chk(b, a);
-  if (m >= 3 && n == 2) flag &= hull_line_chk(b, a);
-  return flag;
+  if (n == 2 && m >= 3) return hull_line_chk(b, a);
+  if (n >= 3 && m == 2) return hull_line_chk(a, b);
+  if (n >= 3 && m >= 3) return hull_hull_chk(a, b) && hull_hull_chk(b, a);
+  return false;
 }
 
 int main() {
