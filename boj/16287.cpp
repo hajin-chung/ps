92a774efa5c47906202a637c9d0ec7b872fad543
hajin-chung <hajinchung1@gmail.com>
2024-10-30 19:21:36 +0900

    sync

--- boj/16287.cpp
+++ boj/16287.cpp
@@ -1,26 +1,45 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-unordered_map<int, int> mp;
+typedef unordered_map<int, bool> uib;
+
+uib rmp;
+vector<uib> mp;
 vector<int> weights;
-int n, sum;
+int n, goal;
+
+void print(uib m) {
+  for (auto [i, b] : m) printf("%d ", i);
+  printf("\n");
+}
 
 int main() {
-  scanf("%d %d", &sum, &n);
+  scanf("%d %d", &goal, &n);
   for (int i = 0; i < n; i++) {
     int w;
     scanf("%d", &w);
     weights.push_back(w);
   }
-
-  for (int i = 0; i < n; i++)
+  mp.resize(n);
+  for (int i = 1 ; i < n; i++) 
     for (int j = 0; j < i; j++) {
-      if (mp[sum - (weights[i]+weights[j])]) {
+      int sum = weights[i] + weights[j];
+      mp[i][sum] = true;
+    }
+
+  for (int i = 2; i < n; i++) {
+    rmp.merge(mp[i-1]);
+    /*printf("%d\n", i);*/
+    /*print(rmp);*/
+    for (int j = i+1; j < n; j++) {
+      int sum = weights[i] + weights[j];
+      if (rmp.count(goal-sum)) {
         printf("YES\n");
         return 0;
       }
-      mp[weights[i]+weights[j]] = 1;
     }
+  }
+
   printf("NO\n");
   return 0;
 }
