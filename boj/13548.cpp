10780a037bec180fe6df785b978aed6a8cb72783
hajin-chung <hajinchung1@gmail.com>
2024-11-01 18:35:18 +0900

    sync

--- boj/13548.cpp
+++ boj/13548.cpp
@@ -1,35 +1,81 @@
 #include <bits/stdc++.h>
+#define MAX 100000
 using namespace std;
 
-int sqrtN;
+int sn;
 struct Q {
-  int idx, s, e;
+  int s, e, idx;
   bool operator<(Q &x) {
-    if (s/sqrtN != x.s/sqrtN) return s/sqrtN < x.s/sqrtN;
+    if (s/sn != x.s/sn) return s/sn < x.s/sn; 
     return e < x.e;
   }
 };
 
+int n, m;
+int a[MAX+5], cnt[MAX+5], ccnt[MAX+5];
 vector<Q> query;
-vector<int> a, ans;
-int n, m, cnt[100005], mxcnt, mx;
+vector<int> ans;
 
 int main() {
   scanf("%d", &n);
-  sqrtN = sqrt(n);
-  a.resize(n+1);
+  sn = sqrt(n);
   for (int i = 1; i <= n; i++) scanf("%d", &a[i]);
   scanf("%d", &m);
   ans.resize(m);
+  int s, e;
   for (int i = 0; i < m; i++) {
-    int s, e;
     scanf("%d %d", &s, &e);
-    query.push_back({i, s, e});
+    query.push_back({s, e, i});
   }
   sort(query.begin(), query.end());
-  mx = mxcnt = 0;
-  for (int i = query[0].s; i <= query[0].e; i++) {
+
+  Q q = query[0];
+  s = q.s, e = q.e;
+  ccnt[0] = n;
+  int mx = 0;
+  for (int i = s; i <= e; i++) {
     cnt[a[i]]++;
+    ccnt[cnt[a[i]]-1]--;
+    ccnt[cnt[a[i]]]++;
     mx = max(mx, cnt[a[i]]);
   }
+  ans[q.idx] = mx;
+
+  for (int i = 1; i < m; i++) {
+    int nmx = 0;
+    q = query[i];
+    while (s < q.s) {
+      cnt[a[s++]]--;
+      ccnt[cnt[a[s-1]]+1]--;
+      ccnt[cnt[a[s-1]]]++;
+      if (ccnt[cnt[a[s-1]]+1]) nmx = max(nmx, cnt[a[s-1]+1]);
+      if (ccnt[cnt[a[s-1]]]) nmx = max(nmx, cnt[a[s-1]]);
+    }
+    while (s > q.s) {
+      cnt[a[--s]]++;
+      ccnt[cnt[a[s]]-1]--;
+      ccnt[cnt[a[s]]]++;
+      if (ccnt[cnt[a[s]]-1]) nmx = max(nmx, cnt[a[s]]-1);
+      if (ccnt[cnt[a[s]]]) nmx = max(nmx ,cnt[a[s]]);
+    }
+    while (e < q.e) {
+      cnt[a[++e]]++;
+      ccnt[cnt[a[e]]-1]--;
+      ccnt[cnt[a[e]]]++;
+      if (ccnt[cnt[a[e]]-1]) nmx = max(nmx, cnt[a[e]]-1);
+      if (ccnt[cnt[a[e]]]) nmx = max(nmx, cnt[a[e]]);
+    }
+    while (e > q.e) {
+      cnt[a[e--]]--;
+      ccnt[cnt[a[e+1]]+1]--;
+      ccnt[cnt[a[e+1]]]++;
+      if (ccnt[cnt[a[e+1]]+1]) nmx = max(nmx, cnt[a[e+1]]+1);
+      if (ccnt[cnt[a[e+1]]]) nmx = max(nmx, cnt[a[e+1]]);
+    }
+    if (ccnt[mx] == 0) mx = nmx;
+    else mx = max(mx, nmx);
+    ans[q.idx] = mx;
+  }
+
+  for (auto aa : ans) printf("%d\n", aa);
 }
---
+++ boj/4949.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  while (true) {
+    string line;
+    getline(cin, line);
+    if (line.size() == 1 && line[0] == '.') break;
+    stack<char> s;
+    bool flag = true;
+    for (auto c : line) {
+      if (c == '(') s.push('(');
+      else if (c == ')') {
+        if (s.empty()) flag = false;
+        else if (s.top() != ')') flag = false;
+        else s.pop();
+      }
+      else if (c == '[') s.push('[');
+      else if (c == ']') {
+        if (s.empty()) flag = false;
+        else if (s.top() != ']') flag = false;
+        else s.pop();
+      }
+    }
+    if (flag) cout << "yes\n";
+    else cout << "no\n";
+  }
+}
