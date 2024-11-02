5fd329fe1ea302c31efbccd8ffcb9ae67f755f76
hajin-chung <hajinchung1@gmail.com>
2024-11-02 17:37:52 +0900

    sync

--- boj/2108.cpp
+++ boj/2108.cpp
@@ -3,22 +3,25 @@
 using namespace std;
 
 vector<int> a;
+vector<int> frq;
 int cnt[8005];
 int n;
 
 int main() {
   ios::sync_with_stdio(0); cin.tie(0);
   double sum = 0;
-  int frq = 0, mn = 4005, mx = 0;
+  int mf = 0, mn = 4005, mx = 0;
   scanf("%d", &n); a.resize(n); 
   for (auto &x : a) {
     cin >> x;
     cnt[x+4000]++;
     sum += (double)x;
-    if (cnt[frq+4000] < cnt[x+4000]) frq = x;
+    if (cnt[mf+4000] < cnt[x+4000]) mf = x;
     mn = min(mn, x);
     mx = max(mx, x);
   }
+  for (auto x : a) if (cnt[x+4000] == cnt[mf+4000]) frq.push_back(x);
+  sort(all(frq));
   sort(all(a));
-  cout << (int)(sum/n+0.5) << "\n" << a[n/2] << "\n" << frq << "\n" << mx-mn << "\n";
+  cout << (int)(sum/n+0.5) << "\n" << a[n/2] << "\n" << frq[1] << "\n" << mx-mn << "\n";
 }
