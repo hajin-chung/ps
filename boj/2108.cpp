c6022556e4c1d65bf3f69da4726e9457740029ef
hajin-chung <hajinchung1@gmail.com>
2024-11-02 17:36:22 +0900

    sync

--- boj/2108.cpp
+++ boj/2108.cpp
@@ -8,16 +8,17 @@ int n;
 
 int main() {
   ios::sync_with_stdio(0); cin.tie(0);
-  int sum = 0, frq = 0, mn = 4005, mx = 0;
+  double sum = 0;
+  int frq = 0, mn = 4005, mx = 0;
   scanf("%d", &n); a.resize(n); 
   for (auto &x : a) {
     cin >> x;
     cnt[x+4000]++;
-    sum += x;
+    sum += (double)x;
     if (cnt[frq+4000] < cnt[x+4000]) frq = x;
     mn = min(mn, x);
     mx = max(mx, x);
   }
   sort(all(a));
-  cout << (int)round((double)sum/n) << "\n" << a[n/2] << "\n" << frq << "\n" << mx-mn << "\n";
+  cout << (int)(sum/n+0.5) << "\n" << a[n/2] << "\n" << frq << "\n" << mx-mn << "\n";
 }
