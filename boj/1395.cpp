b6e69bb94009bd93f8c6481f128df8c842916aff
hajin-chung <hajinchung1@gmail.com>
2024-11-03 16:50:09 +0900

    sync

--- boj/1395.cpp
+++ boj/1395.cpp
@@ -12,12 +12,11 @@ void propagate(int node, int l, int r) {
       lazy[node*2]++;
       lazy[node*2+1]++;
     }
-    tree[node] = 0;
+    lazy[node] = 0;
   } 
 }
 
 void update(int node, int l, int r, int ql, int qr) {
-  /*cout << "update: " << node << " " << l << " "  << r << " " << ql << " " << qr << "\n";*/
   propagate(node, l, r);
   if (r < ql || qr < l) return; 
   if (ql <= l && r <= qr) {
@@ -53,7 +52,5 @@ int main() {
     cin >> q >> l >> r;
     if (q == 0) update(1, 1, n, l, r);
     else cout << query(1, 1, n, l, r) << "\n";
-    for (int i = 1; i <= n; i++) cout << query(1, 1, n, i, i) << " ";
-    cout << "\n";
   }
 }
