4221c75087096e41864f27a9ab0ccac702ef400b
hajin-chung <hajinchung1@gmail.com>
2024-11-08 18:59:55 +0900

    sync

--- boj/7579.cpp
+++ boj/7579.cpp
@@ -1,28 +1,29 @@
 #include <bits/stdc++.h>
+#define NC 10000
 using namespace std;
 
 int n, m;
-int w[105], c[105], dp[10005];
+int w[105], c[105], dp[NC+5];
 
 int main() {
   ios::sync_with_stdio(0); cin.tie(0);
   cin >> n >> m;
   for (int i = 0; i < n; i++) cin >> w[i];
   for (int i = 0; i < n; i++) cin >> c[i];
-  for (int i = 0; i < 10000; i++) dp[i] = -1;
+  for (int i = 0; i <= NC; i++) dp[i] = -1;
   dp[0] = 0;
 
   for (int i = 0; i < n; i++) {
-    int tmp[10005];
+    int tmp[NC+5];
     memcpy(tmp, dp, sizeof(dp));
-    for (int j = 0; j < 10000; j++) {
+    for (int j = 0; j <= NC; j++) {
       if (dp[j] == -1) continue;
       dp[j+c[i]] = max(tmp[j+c[i]], tmp[j]+w[i]);
     }
   }
 
   int ans = 0;
-  for (int i = 0; i < 10000; i++)
+  for (int i = 0; i <= NC; i++)
     if (dp[i] >= m) {
       ans = i;
       break;
