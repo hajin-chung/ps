fc27a2b0f35c01211e8e3e324190b3d1112ea8f6
hajin-chung <hajinchung1@gmail.com>
2024-11-09 13:23:37 +0900

    sync

--- boj/2056.cpp
+++ boj/2056.cpp
@@ -6,14 +6,16 @@ int n, dp[10005];
 int main() {
   ios::sync_with_stdio(0); cin.tie(0);
   cin>>n;
+  int ans = 0;
   for (int i = 1; i <= n; i++) {
     int t, m, mt = 0, j;
     cin>>t>>m;
-    while (m--) {
+    for (int k = 0; k < m; k++) {
       cin>>j;
       mt = max(mt, dp[j]);
     }
     dp[i] = mt+t;
+    ans = max(ans, dp[i]);
   }
-  cout << dp[n] << "\n";
+  cout << ans << "\n";
 }
