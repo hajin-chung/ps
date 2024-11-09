e2bf1cf9dff8d2d0528be4968230f5fee901aeec
hajin-chung <hajinchung1@gmail.com>
2024-11-09 12:59:48 +0900

    sync

--- boj/13398.cpp
+++ boj/13398.cpp
@@ -1,22 +1,18 @@
-#include <stdio.h>
-#include <algorithm>
+#include <bits/stdc++.h>
 using namespace std;
 
-int n;
-int sum[100005];
-int msum[100005];
-int a[100005];
+int n, dp[100005][2];
 
 int main() {
-  int i;
-
-  scanf("%d",  &n);
-  for(i=1; i<=n; i++) {
-    scanf("%d", &a[i]);
-    sum[i] = sum[i-1] + a[i];
-    msum[i] = min(msum[i-1], sum[i-1]);
+  cin>>n;
+  int ans = -1000;
+  dp[0][0] = dp[0][1] = -1000;
+  for (int i = 1; i <= n; i++) {
+    int x;
+    cin>>x;
+    dp[i][0] = max(dp[i-1][0], 0)+x;
+    dp[i][1] = max(dp[i-1][1]+x, dp[i-1][0]); 
+    ans = max(ans, max(dp[i][0], dp[i][1]));
   }
-
-  // sum from a to b = s[b] - s[a-1];
-  // maximize sum to b => find minimum of s[a-1];
+  cout << ans << "\n";
 }
