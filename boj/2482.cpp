f84d88ef103c671b9b44650e364ac75dfb3119f3
hajin-chung <hajinchung1@gmail.com>
2024-11-10 13:22:12 +0900

    sync

--- boj/2482.cpp
+++ boj/2482.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int n, k;
-int dp[1005][505];
+int dp[1005][1005];
 
 int main() {
   scanf("%d%d",&n,&k);
@@ -11,6 +11,6 @@ int main() {
   dp[3][1] = 3;
   for (int i = 4; i <= n; i++)
     for (int j = 1; j <= i/2; j++)
-      dp[i][j] = dp[i-1][j] + dp[i-2][j-1];
+      dp[i][j] = (dp[i-1][j] + dp[i-2][j-1])%1000000003;
   printf("%d\n",dp[n][k]);
 }
