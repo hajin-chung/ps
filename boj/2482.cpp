41d44409f7d9a6e2c6c95215b6eaae078f4bd16d
hajin-chung <hajinchung1@gmail.com>
2024-11-10 13:21:26 +0900

    sync

---
+++ boj/2482.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, k;
+int dp[1005][505];
+
+int main() {
+  scanf("%d%d",&n,&k);
+  dp[1][1] = 1;
+  dp[2][1] = 2;
+  dp[3][1] = 3;
+  for (int i = 4; i <= n; i++)
+    for (int j = 1; j <= i/2; j++)
+      dp[i][j] = dp[i-1][j] + dp[i-2][j-1];
+  printf("%d\n",dp[n][k]);
+}
