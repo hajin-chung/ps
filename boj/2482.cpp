d664037a706f17cfe6e61178041ea7ca2f579262
hajin-chung <hajinchung1@gmail.com>
2024-11-10 13:23:13 +0900

    sync

--- boj/2482.cpp
+++ boj/2482.cpp
@@ -9,6 +9,7 @@ int main() {
   dp[1][1] = 1;
   dp[2][1] = 2;
   dp[3][1] = 3;
+  for (int i = 0; i <= n; i++) dp[i][0] = 1;
   for (int i = 4; i <= n; i++)
     for (int j = 1; j <= i/2; j++)
       dp[i][j] = (dp[i-1][j] + dp[i-2][j-1])%1000000003;
