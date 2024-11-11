ced952d4e9024f07921f5f69c50cb69b7658af0c
hajin-chung <hajinchung1@gmail.com>
2024-11-11 18:10:43 +0900

    sync

--- boj/3197.cpp
+++ boj/3197.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 typedef pair<int, int> pii;
 typedef pair<int, pii> pip;
-int n, m, d[1505][1505];
+int n, m, wd[1505][1505], d[1505][1505];
 int dy[4] = {1, 0, -1, 0}, dx[4] = {0, 1, 0, -1};
 char a[1505][1505];
 priority_queue<pip, vector<pip>, greater<pip>> q;
@@ -14,10 +14,13 @@ pii s, e;
 
 int main() {
   scanf("%d%d",&n,&m);
+  for (int i = 1; i <= n; i++)
+    for (int j = 1; j <= m; j++)
+      wd[i][j] = d[i][j] = INF;
   bool flag = false;
   for (int i = 1; i <= n; i++) {
     scanf("%s", &a[i][1]);
-    for (int j = 1; j <= m; j++)
+    for (int j = 1; j <= m; j++) {
       if (a[i][j] == 'L') {
         a[i][j] = '.';
         if (!flag) {
@@ -27,11 +30,27 @@ int main() {
           e = {i, j};
         }
       }
+      if (a[i][j] == '.') {
+        wd[i][j] = 0;
+        q.push({0, {i, j}});
+      }
+    }
   }
-  for (int i = 1; i <= n; i++)
-    for (int j = 1; j <= m; j++)
-      d[i][j] = INF;
   d[s.fi][s.se] = 0;
+  while (!q.empty()) {
+    auto [dist, curr] = q.top(); q.pop();
+    auto [yy, xx] = curr;
+    if (dist > wd[yy][xx]) continue;
+    for (int i = 0; i < 4; i++) {
+      int ty = yy + dy[i];
+      int tx = xx + dx[i];
+      if (!(0 < ty && ty <= n && 0 < tx && tx <= m)) continue;
+      if (wd[ty][tx] > wd[yy][xx] + (a[ty][tx] == 'X')) {
+        wd[ty][tx] = wd[yy][xx] + (a[ty][tx] == 'X');
+        q.push({wd[ty][tx], {ty, tx}});
+      }
+    }
+  }
   q.push({0, s});
   while (!q.empty()) {
     auto [dist, curr] = q.top(); q.pop();
@@ -41,11 +60,11 @@ int main() {
       int ty = yy + dy[i];
       int tx = xx + dx[i];
       if (!(0 < ty && ty <= n && 0 < tx && tx <= m)) continue;
-      if (d[ty][tx] > d[yy][xx] + (a[ty][tx] == 'X')) {
-        d[ty][tx] = d[yy][xx] + (a[ty][tx] == 'X');
+      if (d[ty][tx] > max(d[yy][xx], wd[ty][tx])) {
+        d[ty][tx] = max(d[yy][xx], wd[ty][tx]);
         q.push({d[ty][tx], {ty, tx}});
       }
     }
   }
-  printf("%d\n", (int)ceil((double)d[e.fi][e.se]/2));
+  printf("%d\n", d[e.fi][e.se]);
 }
