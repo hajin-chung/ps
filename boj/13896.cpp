a9aa323f6b17d2e11c0cee2a5268d1152ad12d76
hajin-chung <hajinchung1@gmail.com>
2024-11-16 10:21:14 +0900

    sync

---
+++ boj/13896.cpp
@@ -0,0 +1,97 @@
+// anc = lca(capital, curr)
+// ancnext = child of anc on the route to capital
+// if depth[anc] < depth[curr] => childcnt[curr]
+// else depth[anc] == depth[curr] => n - childcnt[ancnext] 
+
+#include <bits/stdc++.h>
+#define N 100000
+#define LOG_N 20
+#define pb push_back
+using namespace std;
+
+typedef pair<int, int> pii;
+int dp[N+1][LOG_N];
+vector<vector<int>> adj; 
+vector<int> cnt, depth;
+vector<bool> chk;
+
+int dfs(int curr, int d) {
+  depth[curr] = d;
+  cnt[curr] = 1;
+  for (auto next : adj[curr])
+    if (!chk[next]) {
+      chk[next] = true;
+      dp[next][0] = curr;
+      cnt[curr] += dfs(next, d+1);
+    }
+  return cnt[curr];
+}
+
+pii lca(int u, int v) {
+  if (depth[u] < depth[v]) swap(u, v);
+  for (int i = LOG_N-1; i >= 0; i--)
+    if (depth[dp[u][i]] <= depth[v])
+      u = dp[u][i];
+  if (u == v) return {v, 0};
+  for (int i = LOG_N-1; i >= 0; i--)
+    if (dp[u][i] != dp[v][i]) {
+      u = dp[u][i];
+      v = dp[v][i];
+    }
+  return {dp[u][0], u};
+}
+
+void solve(int tc) {
+  int n, q, r;
+  cin>>n>>q>>r;
+  adj.clear(); adj.resize(n+1);
+  chk.clear(); chk.resize(n+1);
+  cnt.clear(); cnt.resize(n+1);
+  depth.clear(); depth.resize(n+1);
+  for (int i = 1; i <= n; i++)
+    for (int j = 0; j < LOG_N; j++)
+      dp[i][j] = 0;
+  for (int i = 0; i < n-1; i++) {
+    int u, v;
+    cin>>u>>v;
+    adj[u].pb(v);
+    adj[v].pb(u);
+  }
+
+  chk[r] = 1;
+  dfs(r, 0);
+  for (int i = 1; i < LOG_N; i++)
+    for (int j = 1; j <= n; j++)
+      dp[j][i] = dp[dp[j][i-1]][i-1];
+
+  /*for (int i = 1; i <= n; i++) {*/
+  /*  cout << i << ": ";*/
+  /*  for (int j = 0; j < 7; j++)*/
+  /*    cout << dp[i][j] << " ";*/
+  /*  cout << endl;*/
+  /*}*/
+  /*cout << "counts:" <<endl;*/
+  /*for (int i = 1; i <= n; i++)*/
+  /*  cout << cnt[i] << " ";*/
+  /*cout << endl;*/
+
+  cout<<"Case #"<<tc<<":\n";
+  while (q--) {
+    int s, u;
+    cin>>s>>u;
+    if (s == 0) r = u;
+    else {
+      auto [anc, ancnext] = lca(r, u);
+      if (r == u) cout<<n<<"\n";
+      else if (anc == u) cout<<n-cnt[ancnext]<<"\n";
+      else cout<<cnt[u]<<"\n";
+    }
+  }
+}
+
+int main() {
+  ios::sync_with_stdio(0); cin.tie(0);
+  int t;
+  cin>>t;
+  for (int i = 1; i <= t; i++) solve(i);
+}
