3baba9ad8c2079423f34ffda3f1d5eeaa1a146c3
hajin-chung <hajinchung1@gmail.com>
2024-11-17 16:40:17 +0900

    sync

---
+++ boj/12728.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long double ld;
+
+ld sanitize(ld x) {
+  x = x-((int)x/1000)*1000;
+  return x;
+}
+
+ld pow(ld x, int n) {
+  if (n == 0) return 1.0;
+  if (n == 1) return x;
+  if (n%2 == 0) {
+    ld y = pow(x, n/2);
+    return sanitize(y*y);
+  } else {
+    ld y = pow(x, (n-1)/2);
+    return sanitize(x*y*y);
+  }
+}
+
+int main() {
+  int t; scanf("%d", &t);
+  ld x = 3.0+sqrt(5);
+  while (t--) {
+    int n; cin>>n;
+    printf("%03d\n", (int)pow(x, n));
+  }
+}
