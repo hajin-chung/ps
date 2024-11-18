489b8075e9f0b1554b90c4f9a89600ab5da6281f
hajin-chung <hajinchung1@gmail.com>
2024-11-18 20:29:27 +0900

    sync

--- boj/17526.cpp
+++ boj/17526.cpp
@@ -19,9 +19,22 @@ double intersect(Line a, Line b) {
 
 void insert(ll p, ll q) {
   Line f = {p, q, 0}; 
+  while (lines.size() >= 2) {
+    Line first = lines.back();
+    Line second = lines[lines.size()-2];
+    double i1 = intersect(second, first);
+    double i2 = intersect(first, f);
+    if (i1 >= i2) lines.pop_back();
+    else break;
+  }
+  if (!lines.empty()) f.s = intersect(lines.back(), f);
+  lines.push_back(f);
 }
 
 ll query(ll x) {
+  while (lines.size() >= 2 && lines[0].f(x) >= lines[1].f(x))
+    lines.pop_front();
+  return lines.front().f(x);
 }
 
 int main() {
@@ -39,7 +52,6 @@ int main() {
   for (int i = 1; i <= n; i++) {
     insert(p[i-1], -s[i-1]*p[i-1]+q[i-1]+dp);
     dp = query(s[i]);
-    cout << dp << endl;
   }
   cout<<dp<<"\n";
 }
