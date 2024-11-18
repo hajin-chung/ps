38bc0a484c8e473a0782e79f0ae9911d76faa2c2
hajin-chung <hajinchung1@gmail.com>
2024-11-18 20:02:49 +0900

    sync

---
+++ boj/17526.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+#define fi first
+#define se second
+#define all(x) (x).begin(), (x).end()
+using namespace std;
+
+typedef long long int ll;
+typedef pair<ll, ll> pll;
+struct Line { // f(x) = px+q, x >= s
+  ll p, q; 
+  double s; 
+  ll f(ll x) { return p*x + q; }
+}; 
+vector<Line> lines;
+
+double intersect(Line a, Line b) {
+  return (double)(b.q-a.q)/(a.p-b.p);
+}
+
+void insert(ll p, ll q) {
+  Line f = {p, q, 0}; 
+  while (!lines.empty()) {
+    f.s = intersect(lines.back(), f);
+    if (lines.back().s < f.s) break;
+    lines.pop_back();
+  }
+  lines.push_back(f);
+}
+
+ll query(ll x) {
+  int l = 0, r = lines.size()-1;
+  while (l<r) {
+    int m = (l+r)>>1;
+    if (x < lines[m].s) r = m;
+    else l = m+1;
+  } 
+  if (l > 0 && x < lines[l].s) return lines[l-1].f(x);
+  return lines[l].f(x);
+}
+
+int main() {
+  ios::sync_with_stdio(0); cin.tie(0); 
+  int n; cin>>n; n--;
+  vector<ll> s(n+1);
+  for (int i = 1; i <= n; i++) {
+    ll x; cin>>x;
+    s[i] = s[i-1]+x;
+  }
+  vector<ll> p(n), q(n);
+  for (int i = 0; i < n; i++)
+    cin>>q[i]>>p[i];
+  ll dp = 0;
+  for (int i = 1; i <= n; i++) {
+    insert(p[i-1], -s[i-1]*p[i-1]+q[i-1]+dp);
+    dp = query(s[i]);
+    cout << dp << endl;
+  }
+  cout<<dp<<"\n";
+}
