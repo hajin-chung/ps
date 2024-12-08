e8e29e187b18b64beeab157f0976fa53244e0b51
hajin-chung <hajinchung1@gmail.com>
2024-12-08 13:06:16 +0900

    sync

---
+++ boj/23062.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+#define INF -10000000000
+using namespace std;
+
+typedef long long int ll;
+
+ll mod(ll a, ll n) {
+  return (a%n+n)%n;
+}
+
+ll ext_gcd(ll a, ll b, ll &x, ll &y) {
+  if (b == 0) {
+    x = 1; y = 0;
+    return a;
+  }
+  ll x1, y1;
+  ll d = ext_gcd(b, a%b, x1, y1); 
+  x = y1; 
+  y = x1 - y1 * a / b;
+  return d;
+}
+
+pair<ll, ll> crt(ll n1, ll r1, ll n2, ll r2) {
+  ll x, y;
+  ll g = ext_gcd(n1, n2, x, y);
+  cout << n1 << " " << r1 << " " << n2 << " " << r2;
+  cout << endl << g << " " << x << " " << y << endl;
+  if ((r2-r1)%g != 0) return {INF, -1};
+  ll diff = (r2-r1)/g;
+  ll n1p = n1 / g;
+  ll n2p = n2 / g;
+  ll X = r1 + x * diff * n1p;
+  ll m = n1p * n2;
+  X %= m;
+  return {m, X};
+}
+
+void solve(ll A, ll B, ll C, ll a, ll b, ll c) {
+  auto [m1, r1] = crt(A, a, B, b); 
+  if (m1 == INF && r1 == -1) {
+    cout << "-1" << endl;
+    return;
+  }
+  auto [m2, r2] = crt(m1, r1, B, b); 
+  if (m2 == INF && r2 == -1) {
+    cout << "-1" << endl;
+    return;
+  }
+  cout << r2 << endl;
+}
+
+int main() {
+  ios::sync_with_stdio(0); cin.tie(0);
+  ll t;
+  cin>>t;
+  while (t--) {
+    ll A, B, C, a, b, c;
+    cin>>A>>B>>C>>a>>b>>c;
+    solve(A, B, C, a, b, c);
+  }
+}
