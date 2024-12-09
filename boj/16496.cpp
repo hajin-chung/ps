2a863d5c24fa7a3d1abef3de893fbd0d49f570ab
hajin-chung <hajinchung1@gmail.com>
2024-12-09 19:33:50 +0900

    sync

--- boj/16496.cpp
+++ boj/16496.cpp
@@ -32,7 +32,15 @@ bool cmp(ll a, ll b) {
 int main() {
   ios::sync_with_stdio(0); cin.tie(0);
   cin>>n; a.resize(n);
-  for (int i = 0; i < n; i++) cin>>a[i];
+  bool zflag = true;
+  for (int i = 0; i < n; i++) {
+    cin>>a[i];
+    if (a[i] != 0) zflag = false;
+  }
+  if (zflag) {
+    cout << "0\n";
+    return 0;
+  }
   sort(a.begin(), a.end(), cmp);
   for (int i = 0; i < n; i++)
     cout<<a[i];
