fc209df48cbb96a579b2253a9c5c4d63ad23aa0f
hajin-chung <hajinchung1@gmail.com>
2024-12-11 19:46:48 +0900

    sync

--- boj/11000.cpp
+++ boj/11000.cpp
@@ -5,31 +5,17 @@ using namespace std;
 
 typedef pair<int, int> pii;
 
-bool comp(pii a, pii b) {
-  if (a.se == b.se) return a.fi < b.fi;
-  return a.se < b.se;
-}
-
 int main() {
   ios::sync_with_stdio(0); cin.tie(0);
   int n; cin>>n;
   vector<pii> a(n), b;
   for (auto &[f, s] : a) cin>>f>>s;
-  sort(a.begin(), a.end(), comp);
-  int ans = 0;
-  while (!a.empty()) {
-    int ls = a[0].se;
-    ans++;
-    for (int i = 1; i < a.size(); i++) {
-      if (a[i].fi >= ls) {
-        ls = a[i].se;
-      } else {
-        b.push_back(a[i]);
-      }
-    }
-    a = b;
-    b.clear();
-    sort(a.begin(), a.end(), comp);
+  sort(a.begin(), a.end());
+  priority_queue<int, vector<int>, greater<int>> pq;
+  pq.push(a[0].se);
+  for (int i = 1; i < n; i++) {
+    if (pq.top() <= a[i].fi) pq.pop();
+    pq.push(a[i].se);
   }
-  cout<<ans<<"\n";
+  cout<<pq.size()<<"\n";
 }
