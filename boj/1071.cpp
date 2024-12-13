a152bd018f57b87c3bd1df81ca843e5603bccc47
hajin-chung <hajinchung1@gmail.com>
2024-12-13 19:52:54 +0900

    sync

--- boj/1071.cpp
+++ boj/1071.cpp
@@ -1,47 +1,36 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-typedef pair<int, int> pii;
-int n, cnt[1005];
-
-void print(int i, int c) { 
-  while (c--) cout<<i<<" ";
-  cout<<"\n";
-}
+int n;
 
 int main() {
   ios::sync_with_stdio(0); cin.tie(0);
   int x; cin>>n;
+  priority_queue<int, vector<int>, greater<int>> pq;
   for (int i = 0; i < n; i++) {
     cin>>x;
-    cnt[x]++;
+    pq.push(x);
   }
-  priority_queue<pii, vector<pii>, greater<pii>> pq;
-  for (int i = 0; i <= 1000; i++) 
-    if (cnt[i])
-      pq.push({i, cnt[i]});
-
-  vector<pii> ans;
-  auto [f, fc] = pq.top(); pq.pop();
-  ans.push_back({f, fc});
+  vector<int> ans;
+  int f = pq.top(); pq.pop();
+  ans.push_back(f);
   while (!pq.empty()) {
-    int f = ans.back().first;
-    auto [s, sc] = pq.top(); pq.pop();
+    int f = ans.back();
+    int s = pq.top(); pq.pop();
     if (f+1 == s) {
       if (pq.empty()) {
-        auto [f, fc] = ans.back(); ans.pop_back();
-        ans.push_back({s, sc});
-        ans.push_back({f, fc});
+        int f = ans.back(); ans.pop_back();
+        ans.push_back(s);
+        ans.push_back(f);
         continue;
       } else {
-        auto [t, tc] = pq.top(); pq.pop();
-        ans.push_back({t, tc});
+        int t = pq.top(); pq.pop();
+        ans.push_back(t);
       }
     }
-    ans.push_back({s, sc});
+    ans.push_back(s);
   }
-  for (auto [i, c] : ans) 
-    while(c--)
-      cout<<i<<" ";
+  for (auto i : ans) 
+    cout<<i<<" ";
   cout<<"\n";
 }
