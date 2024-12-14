a9ec5559357c3dc718ca4d5200f9247e57c6f05a
hajin-chung <hajinchung1@gmail.com>
2024-12-14 19:47:18 +0900

    sync

---
+++ boj/10775.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int par[100005];
+
+int find(int u) {
+  if (u == par[u]) return u;
+  return par[u] = find(par[u]);
+}
+
+void merge(int u, int v) {
+  u = find(u); v = find(v);
+  if (u < v) par[v] = u;
+  else par[u] = v;
+}
+
+int main() {
+  ios::sync_with_stdio(0); cin.tie(0);
+  int g, p, x; cin>>g>>p;
+  for (int i = 1; i <= g; i++) par[i] = i;
+  int ans = -1;
+  for (int i = 0; i < p; i++) {
+    cin>>x;
+    if (ans == -1) {
+      x = find(x);
+      if (x == 0) ans = i;
+      else merge(x, x-1);
+    }
+  }
+  cout<<ans<<"\n";
+}
