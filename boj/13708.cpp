65f20eca09c2146634f4894694a9b88fb8e36741
hajin-chung <hajinchung1@gmail.com>
2024-12-15 15:15:03 +0900

    sync

--- boj/13708.cpp
+++ boj/13708.cpp
@@ -2,34 +2,44 @@
 #include <iomanip>
 using namespace std;
 
-struct Point { double x, y; };
+struct pt { double x, y; };
 int n;
-vector<Point> a;
+vector<pt> a;
 
-double dist(Point a, Point b) {
+double dist(pt a, pt b) {
   double dx = a.x-b.x;
   double dy = a.y-b.y;
   return dx*dx+dy*dy;
 }
 
-Point get_circle(Point a, Point b) {
-  Point c = Point {(a.x+b.x)/2, (a.y+b.y)/2};
+pt get_circle(pt a, pt b) {
+  pt c = pt {(a.x+b.x)/2, (a.y+b.y)/2};
   return c;
 }
 
-Point get_circle(Point a, Point b, Point c) {
-  Point center = Point {
-    -((b.x*b.x-a.x*a.x+b.y*b.y-a.y*a.y)*(c.y-b.y)-(b.x*b.x-c.x*c.x+b.y*b.y-c.y*c.y)*(a.y-b.y))/(2*(a.x-b.x)*(c.y-b.y)-2*(c.x-b.x)*(a.y-b.y)),
-    -((b.y*b.y-a.y*a.y+b.x*b.x-a.x*a.x)*(c.x-b.x)-(b.y*b.y-c.y*c.y+b.x*b.x-c.x*c.x)*(a.x-b.x))/(2*(a.y-b.y)*(c.x-b.x)-2*(c.y-b.y)*(a.x-b.x)),
-  };
-  return center;
+pt get_circle(pt a, pt b, pt c) {
+  double aa = a.x*a.x+a.y*a.y;
+  double bb = b.x*b.x+b.y*b.y;
+  double cc = c.x*c.x+c.y*c.y;
+  double dd = 2*(a.x*(b.y - c.y) + b.x*(c.y - a.y) + c.x*(a.y - b.y));
+  if (fabs(dd) < 1e-14) {
+    double ab = dist(a, b);
+    double bc = dist(b, c);
+    double ca = dist(c, a);
+    if (ab > bc && ab > ca) return get_circle(a, b);
+    if (bc > ab && bc > ca) return get_circle(a, b);
+    if (ca > bc && ca > ab) return get_circle(a, b);
+  }
+  double ux = (aa*(b.y-c.y)+bb*(c.y-a.y)+cc*(a.y-b.y))/dd;
+  double uy = (aa*(c.x-b.x)+bb*(a.x-c.x)+cc*(b.x-a.x))/dd;
+  return {ux, uy};
 }
 
 int main() {
   ios::sync_with_stdio(0); cin.tie(0);
   cin>>n; a.resize(n);
   for (auto &[x, y] : a) cin>>x>>y;
-  Point c = a[0];
+  pt c = a[0];
   double r = -1;
   for (int i = 0; i < n; i++) if (dist(c, a[i]) > r) {
     c = a[i]; r = 0;
