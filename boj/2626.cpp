f6040c3016108eb93a3e139d0d5490cd35df2c7b
hajin-chung <hajinchung1@gmail.com>
2024-12-15 15:21:43 +0900

    sync

--- boj/2626.cpp
+++ boj/2626.cpp
@@ -2,13 +2,14 @@
 #include <iomanip>
 using namespace std;
 
-struct pt { double x, y; };
+typedef long double ld;
+struct pt { ld x, y; };
 int n;
 vector<pt> a;
 
-double dist(pt a, pt b) {
-  double dx = a.x-b.x;
-  double dy = a.y-b.y;
+ld dist(pt a, pt b) {
+  ld dx = a.x-b.x;
+  ld dy = a.y-b.y;
   return dx*dx+dy*dy;
 }
 
@@ -18,20 +19,20 @@ pt get_circle(pt a, pt b) {
 }
 
 pt get_circle(pt a, pt b, pt c) {
-  double aa = a.x*a.x+a.y*a.y;
-  double bb = b.x*b.x+b.y*b.y;
-  double cc = c.x*c.x+c.y*c.y;
-  double dd = 2*(a.x*(b.y - c.y) + b.x*(c.y - a.y) + c.x*(a.y - b.y));
+  ld aa = a.x*a.x+a.y*a.y;
+  ld bb = b.x*b.x+b.y*b.y;
+  ld cc = c.x*c.x+c.y*c.y;
+  ld dd = 2*(a.x*(b.y - c.y) + b.x*(c.y - a.y) + c.x*(a.y - b.y));
   if (fabs(dd) < 1e-14) {
-    double ab = dist(a, b);
-    double bc = dist(b, c);
-    double ca = dist(c, a);
+    ld ab = dist(a, b);
+    ld bc = dist(b, c);
+    ld ca = dist(c, a);
     if (ab > bc && ab > ca) return get_circle(a, b);
     if (bc > ab && bc > ca) return get_circle(a, b);
     if (ca > bc && ca > ab) return get_circle(a, b);
   }
-  double ux = (aa*(b.y-c.y)+bb*(c.y-a.y)+cc*(a.y-b.y))/dd;
-  double uy = (aa*(c.x-b.x)+bb*(a.x-c.x)+cc*(b.x-a.x))/dd;
+  ld ux = (aa*(b.y-c.y)+bb*(c.y-a.y)+cc*(a.y-b.y))/dd;
+  ld uy = (aa*(c.x-b.x)+bb*(a.x-c.x)+cc*(b.x-a.x))/dd;
   return {ux, uy};
 }
 
@@ -40,7 +41,7 @@ int main() {
   cin>>n; a.resize(n);
   for (auto &[x, y] : a) cin>>x>>y;
   pt c = a[0];
-  double r = -1;
+  ld r = -1;
   for (int i = 0; i < n; i++) if (dist(c, a[i]) > r) {
     c = a[i]; r = 0;
     for (int j = 0; j < i; j++) if (dist(c, a[j]) > r) {
