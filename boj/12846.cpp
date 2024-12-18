d218b49f8326257a30298be6f584eb24b36add18
hajin-chung <hajinchung1@gmail.com>
2024-12-18 18:31:57 +0900

    clean

---
+++ boj/12846.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long int ll;
+
+int main() {
+  ios::sync_with_stdio(0); cin.tie(0); 
+  int n; cin>>n;
+  vector<ll> a(n); for (auto &ai : a) cin>>ai;
+  a.push_back(0);
+  ll ans = 0;
+  stack<int> s;
+  for (int i = 0; i <= n; i++) {
+    while (!s.empty() && a[s.top()] > a[i]) {
+      ll sz = (ll)(i-s.top()) * a[s.top()];
+      ans = max(ans, sz);
+      s.pop();
+    }
+    s.push(i);
+  }
+  cout<<ans<<"\n";
+}
