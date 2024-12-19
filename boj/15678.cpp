4ad2b7ea42e22b644f41fa8d8f9b8c39496c3e26
hajin-chung <hajinchung1@gmail.com>
2024-12-19 19:06:27 +0900

    clean

--- boj/15678.cpp
+++ boj/15678.cpp
@@ -5,15 +5,18 @@ int main() {
   ios::sync_with_stdio(0); cin.tie(0);
   int n, d; cin>>n>>d;
   vector<int> a(n), dp(n); 
+  multiset<int> s;
   for (auto &ai : a) cin>>ai;
   int ans = INT_MIN;
-  priority_queue<pair<int, int>> pq;
   for (int i = 0; i < n; i++) {
-    while (!pq.empty() && i - pq.top().second > d) pq.pop();
-    if (!pq.empty() && pq.top().first > 0) dp[i] = a[i] + pq.top().first;
-    else dp[i] = a[i];
+    if (i > d) {
+      auto iter = s.find(dp[i-d-1]);
+      if (iter != s.end()) s.erase(iter);
+    }
+    int mx = s.empty() ? 0 : max(0, *s.rbegin());
+    dp[i] = mx + a[i];
+    s.insert(dp[i]);
     ans = max(ans, dp[i]);
-    pq.push({dp[i], i});
   }
   cout<<ans<<"\n";
 }
