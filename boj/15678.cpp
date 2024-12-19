c322a5d9744936eceba697d1ec79cbc18b20fa52
hajin-chung <hajinchung1@gmail.com>
2024-12-19 19:19:40 +0900

    clean

--- boj/15678.cpp
+++ boj/15678.cpp
@@ -1,15 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+typedef long long int ll;
 int main() {
   ios::sync_with_stdio(0); cin.tie(0);
   int n, d; cin>>n>>d;
-  vector<int> a(n), dp(n); 
-  multiset<int> s;
+  vector<ll> a(n), dp(n); 
+  multiset<ll> s;
   for (auto &ai : a) cin>>ai;
-  int ans = INT_MIN;
+  ll ans = LLONG_MIN;
   for (int i = 0; i < n; i++) {
-    int mx = s.empty() ? 0 : max(0, *s.rbegin());
+    ll mx = s.empty() ? 0 : max((ll)0, *s.rbegin());
     dp[i] = mx + a[i];
     if (i >= d) {
       auto iter = s.find(dp[i-d]);
