4057aa7e52ffc42c6b504e0a5fc46c9c61c77824
hajin-chung <hajinchung1@gmail.com>
2024-12-20 18:44:59 +0900

    clean

--- boj/10803.cpp
+++ boj/10803.cpp
@@ -4,12 +4,16 @@ using namespace std;
 int dp[10001][101];
 
 int go(int w, int h) {
+  if (h > w) return go(h, w);
   if (dp[w][h]) return dp[w][h];
   if (w == h) return dp[w][h] = 1;
   int ret = INT_MAX;
+  if (w / h >= 3) {
+    return dp[w][h] = go(w-h, h) + 1;
+  }
   /*cout<<w<<" "<<h<<endl;*/
-  for (int i = 1; i < w; i++) ret = min(ret, go(i, h) + go(w-i, h));
-  for (int i = 1; i < h; i++) ret = min(ret, go(w, i) + go(w, h-i));
+  for (int i = 1; i <= w/2; i++) ret = min(ret, go(i, h) + go(w-i, h));
+  for (int i = 1; i <= h/2; i++) ret = min(ret, go(w, i) + go(w, h-i));
   return dp[w][h] = ret;
 }
 
