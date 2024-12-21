a9687a86c9a50f65c6e53d51061f8f497a0b683a
hajin-chung <hajinchung1@gmail.com>
2024-12-21 15:55:56 +0900

    clean

--- boj/24515.cpp
+++ boj/24515.cpp
@@ -21,42 +21,38 @@ void floodfill(int u, int yy, int xx) {
     for (int i = 0; i < 4; i++) {
       int ty = yy+dy[i];
       int tx = xx+dx[i];
-      if (ty < 0 || ty >= n || tx < 0 || tx >= n) continue;
-      if (s[yy][xx] != s[ty][tx]) continue;
+      if (ty <= 0 || ty > n || tx <= 0 || tx > n) continue;
+      if (s[yy-1][xx-1] != s[ty-1][tx-1]) continue;
       if (a[ty][tx] == 0) Q.push({ty, tx});
     }
   }
 }
 
 void color() {
-  for (int i = 0; i < n; i++)
-    for (int j = 0; j < n; j++)
-      if (s[i][j] != '.' && a[i][j] == 0) {
+  for (int i = 1; i <= n; i++)
+    for (int j = 1; j <= n; j++)
+      if (s[i-1][j-1] != '.' && a[i][j] == 0) {
         sz.push_back(0);
         floodfill(k++, i, j);
       }
 }
 
-int dij_top() {
-  int ret = INF;
+int dij() {
   priority_queue<pip, vector<pip>, greater<pip>> pq;
-  int d[1010][1010];
-  for (int i = 0; i < n; i++)
-    for (int j = 0; j < n; j++) {
-      if (i == 0 && j > 0) {
-        d[i][j] = sz[a[i][j]];
-        pq.push({d[i][j], {i, j}});
-      } else d[i][j] = INF;
-    }
+  int d[1005][1005];
+  for (int i = 0; i <= n+1; i++)
+    for (int j = 0; j <= n+1; j++)
+      d[i][j] = INF;
+  d[0][2] = 0;
+  pq.push({0, {0, 2}});
   while (!pq.empty()) {
     auto [dist, yx] = pq.top(); pq.pop();
     auto [yy, xx] = yx;
-    if (dist > d[yy][xx]) continue; 
+    if (dist > d[yy][xx]) continue;
     for (int i = 0; i < 8; i++) {
       int ty = yy+dy[i];
       int tx = xx+dx[i];
-      if ((ty == 0 && tx == 0) || (ty == n || tx == n)) continue;
-      if (ty < 0 || ty > n || tx < 0 || tx > n) continue;
+      if (ty < 0 || ty > n+1 || tx < 0 || tx > n+1) continue;
       int w = 0;
       if (a[ty][tx] != a[yy][xx]) w = sz[a[ty][tx]];
       if (d[ty][tx] > d[yy][xx] + w) {
@@ -65,42 +61,9 @@ int dij_top() {
       }
     }
   }
-  for (int i = 0; i < n-1; i++)
-    ret = min(ret, d[n-1][i]);
-  for (int i = 0; i < n-1; i++)
-    ret = min(ret, d[i][0]);
-  return ret;
-}
-
-int dij_left() {
   int ret = INF;
-  priority_queue<pip, vector<pip>, greater<pip>> pq;
-  int d[1010][1010];
-  for (int i = 0; i < n; i++)
-    for (int j = 0; j < n; j++) {
-      if (j == 0 && i > 0) {
-        d[i][j] = sz[a[i][j]];
-        pq.push({d[i][j], {i, j}});
-      } else d[i][j] = INF;
-    }
-  while (!pq.empty()) {
-    auto [dist, yx] = pq.top(); pq.pop();
-    auto [yy, xx] = yx;
-    if (dist > d[yy][xx]) continue; 
-    for (int i = 0; i < 8; i++) {
-      int ty = yy+dy[i];
-      int tx = xx+dx[i]; if ((ty == 0 && tx == 0) || (ty == n || tx == n)) continue;
-      if (ty < 0 || ty > n || tx < 0 || tx > n) continue;
-      int w = 0;
-      if (a[ty][tx] != a[yy][xx]) w = sz[a[ty][tx]];
-      if (d[ty][tx] > d[yy][xx] + w) {
-        d[ty][tx] = d[yy][xx] + w;
-        pq.push({d[ty][tx], {ty, tx}});
-      }
-    }
-  }
-  for (int i = 0; i < n-1; i++)
-    ret = min(ret, d[i][n-1]);
+  for (int i = 0; i <= n-1; i++) ret = min(ret, d[n+1][i]);
+  for (int i = 2; i <= n; i++) ret = min(ret, d[i][0]);
   return ret;
 }
 
@@ -109,5 +72,14 @@ int main() {
   cin>>n; s.resize(n);
   for (auto &si: s) cin>>si;
   color();
-  cout<<min(dij_top(), dij_left())<<"\n";
+  sz.push_back(0);
+  for (int i = 2; i <= n+1; i++) a[0][i] = k;
+  for (int i = 1; i <= n-1; i++) a[i][n+1] = k;
+  k++;
+  sz.push_back(0);
+  for (int i = 0; i <= n-1; i++) a[n+1][i] = k;
+  for (int i = 2; i <= n; i++) a[i][0] = k;
+  k++;
+  sz[0] = INF;
+  cout<<dij()<<"\n";
 }
