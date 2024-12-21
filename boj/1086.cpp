72c5215021e6e7ef23b9883969242868151880aa
hajin-chung <hajinchung1@gmail.com>
2024-12-21 13:56:09 +0900

    clean

--- boj/1086.cpp
+++ boj/1086.cpp
@@ -3,16 +3,8 @@ using namespace std;
 
 typedef long long int ll;
 int n, k;
-ll dp[1<<15][105], a[105];
+ll dp[1<<15][105], a[105], ttable[55];
 string s[105];
-unordered_map<int, int> ttable;
-
-int tmod(int n) {
-  if (ttable.count(n)) return ttable[n];
-  int ret = 1;
-  while (n--) ret = (ret*10)%k;
-  return ttable[n] = ret;
-}
 
 ll gcd(ll a, ll b) {
   if (b == 0) return a;
@@ -29,13 +21,17 @@ int main() {
     for (int j = 0; j < m; j++)
       a[i] = (a[i]*10+(s[i][j]-'0'))%k;
   }
+  ttable[1] = 10%k;
+  for (int i = 2; i <= 50; i++) {
+    ttable[i] = (ttable[i-1]*10)%k;
+  }
   dp[0][0] = 1;
   for (int mask = 0; mask < (1<<n); mask++) {
     for (int i = 0; i < n; i++)
       if ((mask & (1<<i)) == 0) {
         int next = mask | (1<<i);
         for (int j = 0; j < k; j++) {
-          int r = ((j*tmod(s[i].length()))%k+a[i])%k;
+          int r = ((j*ttable[s[i].length()])%k+a[i])%k;
           dp[next][r] += dp[mask][j];
         }
       }
