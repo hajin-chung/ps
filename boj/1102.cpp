6af674949da76b09d6b4a1d8c61b4aa28c3d464d
hajin-chung <hajinchung1@gmail.com>
2024-12-22 14:29:06 +0900

    clean

--- boj/1102.cpp
+++ boj/1102.cpp
@@ -2,29 +2,7 @@
 #define INF 1000000000
 using namespace std;
 
-int dp[1<<16], n, a[16][16], p;
-
-int bc(int mask) {
-  int ret = 0;
-  while (mask) {
-    if (mask&1) ret++;
-    mask>>=1;
-  }
-  return ret;
-}
-
-int rec(int mask) {
-  if (dp[mask] != INF) return dp[mask];
-  for (int i = 0; i < n; i++) 
-    if (mask & (1<<i)) {
-      int mn = INF;
-      for (int j = 0; j < n; j++)
-        if (i != j && mask & (1<<j))
-          mn = min(mn, a[j][i]);
-      dp[mask] = min(dp[mask], mn + rec(mask & (~(1<<i))));
-    }
-  return dp[mask];
-}
+int dp[1<<16], n, a[16][16], p, mask;
 
 int main() {
   ios::sync_with_stdio(0); cin.tie(0);
@@ -32,24 +10,32 @@ int main() {
   for (int i = 0; i < n; i++)
     for (int j = 0; j < n; j++)
       cin>>a[i][j];
-  int mask = 0;
   for (int i = 0; i < n; i++) {
-    char c;
-    cin>>c;
-    if (c == 'Y') mask |= (1<<i);
+    char x; cin>>x;
+    if (x == 'Y') mask += (1<<i);
   }
   cin>>p;
-  if (bc(mask) >= p) {
-    cout << 0 << "\n";
-    return 0;
+  if (p == 0) {
+    cout<<"-1\n"; return 0;
+  } else if (__builtin_popcount(mask) >= p) {
+    cout<<0<<"\n"; return 0;
   }
-  for (int i = 0; i < (1<<16); i++) dp[i] = INF;
+  for (int i = 0; i < (1<<n); i++) dp[i] = INF;
   dp[mask] = 0;
   int ans = INF;
-  for (int i = 0; i < (1<<16); i++) {
-    rec(i);
-    if (bc(i) == p) ans = min(ans, dp[i]);
-  }
-  if (ans == INF) cout << -1<< "\n";
-  else cout << ans << "\n";
+  for (int i = mask; i < (1<<n); i++)
+    for (int j = 0; j < n; j++) 
+      if ((i & (1<<j)) == 0) {
+        int q = i | (1<<j), w = INF;
+        for (int k = 0; k < n; k++) 
+          if ((i & 1<<k) != 0 && a[k][j] < w)
+            w = a[k][j];
+        if (dp[q] > dp[i] + a[i][j]) {
+          dp[q] = dp[i] + w;
+          if (__builtin_popcount(q) >= p && ans > dp[q]) {
+            ans = dp[q];
+          }
+        }
+      }
+  cout<<ans<<"\n";
 }
