60e9e353309a54f05cab5691620c4106a5b79d4d
hajin-chung <hajinchung1@gmail.com>
2024-12-24 19:38:22 +0900

    clean

--- boj/13261.cpp
+++ boj/13261.cpp
@@ -11,28 +11,28 @@ void f(int t, int s, int e, int l, int r) {
   int m = (s+e)>>1;
   int opt = -1;
   ll &ans = dp[t][m];
-  for (int i = l; i <= min(r, m); i++) {
-    ll val = dp[t-1][i-1] + (sum[m]-sum[i-1])*(m-i+1);
+  for (int i = l; i < min(r, m); i++) {
+    ll val = dp[t-1][i] + (sum[m]-sum[i])*(m-i);
     if (opt == -1 || ans > val) {
       opt = i;
       ans = val;
     }
   }
-  f(t, s, m-1, l, opt);
+  f(t, s, m-1, l, opt+1);
   f(t, m+1, e, opt, r);
 }
 
 int main() {
   ios::sync_with_stdio(0); cin.tie(0);
   int l, g; cin>>l>>g;
+  if (g >= l) g = l;
   for (int i = 1; i <= l; i++) {
     cin>>a[i];
     sum[i] = sum[i-1] + a[i];
   }
-  if (g >= l) g = l;
   for (int i = 1; i <= l; i++)
     dp[1][i] = sum[i]*i;
   for (int i = 2; i <= g; i++)
-    f(i, i, l, i, l);
+    f(i, 0, l, 0, l);
   cout<<dp[g][l]<<"\n";
 }
