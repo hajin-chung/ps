dcad5bd7d78cd49fd412cc5ea6a017bc7b8d8e9d
hajin-chung <hajinchung1@gmail.com>
2024-12-25 11:11:21 +0900

    clean

--- boj/17613.cpp
+++ boj/17613.cpp
@@ -1,20 +1,31 @@
-/*
- * 1 = 1
- * 2 = 1 + 1
- * 3 = 1 + 2
- * 4 = 1 + 2 + 1
- * 5 = 1 + 2 + 1 + 1 = 2^2-1 + 2^1-1 + 2^1 -1
- * 6 = 1 + 2 + 1 + 2
- * 7 = 1 + 2 + 4
- * 8 = 1 + 2 + 4 + 1
- * 9 = 1 + 2 + 4 + 1 + 1
- * 10 = 1 + 2 + 4 + 1 + 2
- * 11 = 1 + 2 + 4 + 1 + 2 + 1
- * 12 = 1 + 2 + 4 + 1 + 2 + 1 + 1
- * 13 = 1 + 2 + 4 + 1 + 2 + 1 + 2
- * 14 = 1 + 2 + 4 + 1 + 2 + 4
- * 15 = 1 + 2 + 4 + 8
- * 16 = 1 + 2 + 4 + 8 + 1
- * 19 = 1 + 2 + 4 + 8 + 1 + 2 + 1
- * dp[n] = dp[m] + dp[n-m] (m = 2^k-1 < n max k)
-*/
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long int ll;
+typedef pair<ll, ll> pll;
+map<pll, int> dp;
+
+ll f(ll x, ll y) {
+  if (x == 0 && y == 0) return 0; 
+  if (dp.count({x, y})) return dp[{x, y}];
+  ll ret = 0;
+  for (int i = 1; i <= 40; i++) {
+    ll s = (1LL<<i)-1;
+    ll e = (1LL<<(i+1))-2;
+    s = max(s, x); e = min(e, y);
+
+    ll ss = s - (1LL<<i) + 1;
+    ll ee = e - (1LL<<i) + 1;
+    if (s <= e) ret = max(ret, i + f(ss, ee));
+  }
+  return dp[{x, y}] = ret;
+}
+
+int main() {
+  ios::sync_with_stdio(0); cin.tie(0);
+  int t; cin>>t;
+  while (t--) {
+    ll x, y; cin>>x>>y;
+    cout<<f(x, y)<<"\n";
+  }
+}
