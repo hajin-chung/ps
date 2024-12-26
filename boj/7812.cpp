7fdbd2947e65912e25b9a153670b4722521039f6
hajin-chung <hajinchung1@gmail.com>
2024-12-26 18:51:18 +0900

    clean

---
+++ boj/7812.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef pair<int, int> pii;
+int n; 
+
+void count(vector<vector<pii>> &a, vector<int> &ch, vector<bool> &chk, int u, int d, int &sum) {
+  chk[u] = 1;
+  ch[u] = 1;
+  sum += d;
+  for (auto [v, w] : a[u])
+    if (!chk[v]) {
+      count(a, ch, chk, v, d+w, sum); 
+      ch[u] += ch[v];
+    }
+}
+
+void traverse(vector<vector<pii>> &a, vector<int> &ch, vector<bool> &chk, int u, int s, int &mn) {
+  chk[u] = 1;
+  mn = min(mn, s);
+  for (auto [v, w] : a[u])
+    if (!chk[v]) {
+      int ns = s - w*ch[v] + w*(n-ch[v]);
+      traverse(a, ch, chk, v, ns, mn);
+    }
+}
+
+bool solve() {
+  cin>>n;
+  if (n == 0) return false;
+  vector<vector<pii>> a(n);
+  for (int i = 0; i < n-1; i++) {
+    int u, v, w; cin>>u>>v>>w;
+    a[u].push_back({v, w});
+    a[v].push_back({u, w});
+  }
+  vector<int> ch(n);
+  vector<bool> chk(n);
+  int sum = 0;
+  count(a, ch, chk, 0, 0, sum);
+  int mn = sum;
+  for (int i = 0; i < n; i++) chk[i] = 0;
+  traverse(a, ch, chk, 0, sum, mn);
+  cout<<mn<<"\n";
+  return true;
+}
+
+int main() {
+  ios::sync_with_stdio(0); cin.tie(0);
+  while (solve()) {}
+}
