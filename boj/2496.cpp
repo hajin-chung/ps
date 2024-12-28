8b6b12adc34c9e624dd3547b2d25ff6e7d7fcbf9
hajin-chung <hajinchung1@gmail.com>
2024-12-28 18:13:01 +0900

    clean

--- boj/2496.cpp
+++ boj/2496.cpp
@@ -17,28 +17,26 @@ int main() {
   pii p;
   auto f = [&](int sx, int sy) {
     int cnt = 0;
-    int yy = (sx+sy+s)/2;
-    int xx = sx-yy+s/2;
-    /*if (yy < 0 || yy > m || xx < 0 || xx > n) return 0;*/
+    int x = (sx-sy)/2, y = (sx+sy)/2 + s/2;
+    if (x < 0 || x > n || y < 0 || y > m) return;
     for (int k = 0; k < t; k++) 
       if (sx <= a[k].fi 
           && a[k].fi <= sx+s
           && sy <= a[k].se 
           && a[k].se <= sy+s)
         cnt++;
-    return cnt;
+    if (ans < cnt) {
+      ans = cnt; 
+      p = {x, y};
+    }
   };
   for (int i = 0; i < t; i++) 
     for (int j = 0; j < t; j++) {
-      int c = f(a[i].fi, a[j].se);
-      if (c > ans) {
-        ans = c;
-        p = {a[i].fi+s/2, a[j].se+s/2};
-      }
+      int x = a[i].fi, y = a[j].se;
+      if ((x+y)%2) f(x+1, y), f(x-1, y), f(x, y+1), f(x, y-1);
+      else f(x, y);
+      f(x+y-s/2, y-x-s/2);
     }
-  int xx = p.fi, yy = p.se;
-  yy = (xx+yy)/2;
-  xx = xx-yy;
-  cout<<xx<<" "<<yy<<"\n";
+  cout<<p.fi<<" "<<p.se<<"\n";
   cout<<ans<<"\n";
 }
