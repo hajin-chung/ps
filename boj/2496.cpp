6f80eb131fa500b7a16011035e24b0bd54045c0d
hajin-chung <hajinchung1@gmail.com>
2024-12-28 17:18:39 +0900

    clean

--- boj/2496.cpp
+++ boj/2496.cpp
@@ -18,13 +18,13 @@ int main() {
   for (int i = 1; i < t; i++)
     for (int j = 0; j < i; j++) {
       int xx = min(a[i].fi, a[j].fi);
-      int yy = min(a[i].se, a[j].se);
+      int yy = max(a[i].se, a[j].se);
       int cnt = 0;
       for (int k = 0; k < t; k++) 
         if (xx <= a[k].fi 
             && a[k].fi <= xx+s
-            && yy <= a[k].se 
-            && a[k].se <= yy+s)
+            && yy-s <= a[k].se 
+            && a[k].se <= yy)
           cnt++;
       if (cnt > ans) {
         ans = cnt;
@@ -34,7 +34,7 @@ int main() {
   int xx = p.fi, yy = p.se;
   yy = (xx+yy)/2;
   xx = xx-yy;
-  yy += s/2;
+  xx += s/2;
   cout<<xx<<" "<<yy<<"\n";
   cout<<ans<<"\n";
 }
