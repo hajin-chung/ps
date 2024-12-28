d6d4e88d65e79b37295818c4133acd25617aa630
hajin-chung <hajinchung1@gmail.com>
2024-12-28 17:30:10 +0900

    clean

--- boj/2496.cpp
+++ boj/2496.cpp
@@ -15,18 +15,18 @@ int main() {
   }
   int ans = 0;
   pii p;
-  for (int i = 1; i < t; i++)
+  auto f = [&](int sx, int sy) {
+    int cnt = 0;
+    for (int k = 0; k < t; k++) 
+      if (sx <= a[k].fi 
+          && a[k].fi <= sx+s
+          && sy <= a[k].se 
+          && a[k].se <= sy+s)
+        cnt++;
+    return cnt;
+  };
+  for (int i = 1; i < t; i++) 
     for (int j = 0; j < i; j++) {
-      auto f = [&](int sx, int sy) {
-        int cnt = 0;
-        for (int k = 0; k < t; k++) 
-          if (sx <= a[k].fi 
-              && a[k].fi <= sx+s
-              && sy <= a[k].se 
-              && a[k].se <= sy+s)
-            cnt++;
-        return cnt;
-      };
       int c1 = f(a[i].fi, a[j].se);
       int c2 = f(a[j].fi, a[i].se);
       if (c1 > ans) {
@@ -38,6 +38,13 @@ int main() {
         p = {a[j].fi, a[i].se};
       }
     }
+  for (int i = 0; i < t; i++) {
+    int c = f(a[i].fi, a[i].se);
+    if (c > ans) {
+      ans = c;
+      p = a[i];
+    }
+  }
   int xx = p.fi, yy = p.se;
   yy = (xx+yy)/2;
   xx = xx-yy;
