9ebdf6199af604a2ef5518989e3a7a2d4cf5b8ae
hajin-chung <hajinchung1@gmail.com>
2024-12-30 19:52:32 +0900

    clean

--- boj/15708.cpp
+++ boj/15708.cpp
@@ -4,25 +4,21 @@
 using namespace std;
 
 typedef long long int ll;
-typedef pair<ll, ll> pli;
 
 int main() {
   ios::sync_with_stdio(0); cin.tie(0);
   ll n, t, p; cin>>n>>t>>p;
-  priority_queue<pli, vector<pli>, greater<>> pq;
+  priority_queue<ll> pq;
+  ll sum = 0, ans = 0;
   for (ll i = 0; i < n; i++) {
-    ll x; cin>>x;
-    pq.push({x+i*p, i});
+    ll k; cin>>k;
+    sum += k;
+    pq.push(k);
+    while (!pq.empty() && sum + i*p > t) {
+      sum -= pq.top();
+      pq.pop();
+    }
+    ans = max(ans, (ll)pq.size());
   }
-  ll mx = 0, cnt = 0;
-  while (!pq.empty()) {
-    auto [cost, idx] = pq.top(); pq.pop(); 
-    if (idx > mx) cost -= mx*p;
-    else cost -= idx*p;
-    mx = max(idx, mx);
-    if (t < cost) break;
-    t -= cost;
-    cnt++;
-  }
-  cout<<cnt<<"\n";
+  cout<<ans<<"\n";
 }
