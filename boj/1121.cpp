396125a0adb9b9359ac90a6646b906dd879f7cfc
hajin-chung <hajinchung1@gmail.com>
2025-01-01 17:31:57 +0900

    clean

--- boj/1121.cpp
+++ boj/1121.cpp
@@ -1,9 +1,9 @@
 #include <bits/stdc++.h>
-#define MAX 2500000
+#define MAX 50000
 using namespace std;
 
 typedef long long int ll;
-ll dp[10][2500005];
+ll dp[10][MAX+5];
 
 int main() {
   ios::sync_with_stdio(0); cin.tie(0);
@@ -18,11 +18,13 @@ int main() {
   ll cnt = 0;
   for (int i = 0; i < n; i++) {
     if (i + 1 >= k)
-      for (int j = a[i]+1; j <= sum[i-1]; j++)
+      for (int j = a[i]+1; j <= MAX+1; j++)
         cnt += dp[k-1][j];
     for (int kk = k-1; kk > 0; kk--)
-      for (int j = sum[i]; j >= a[i]; j--)
-        dp[kk][j] += dp[kk-1][j-a[i]];
+      for (int j = sum[i]; j >= a[i]; j--) {
+        int u = min(j, MAX+1), v = min(j-(int)a[i], MAX+1);
+        dp[kk][u] += dp[kk-1][v];
+      }
   }
   cout<<cnt<<"\n";
 }
