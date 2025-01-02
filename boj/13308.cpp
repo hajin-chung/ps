d1d03017cbad3187b2cb7cc196dd88e3464cd413
hajin-chung <hajinchung1@gmail.com>
2025-01-02 18:04:55 +0900

    clean

--- boj/13308.cpp
+++ boj/13308.cpp
@@ -1,20 +1,21 @@
 #include <bits/stdc++.h>
-#define INF 1000000000
 using namespace std;
 
-typedef pair<int, int> pii;
-typedef pair<pii, int> ppi;
-int d[2505][2505];
+typedef long long int ll;
+typedef pair<int, ll> pil;
+typedef pair<ll, ll> pll;
+typedef pair<pll, int> ppi;
+ll d[2505][2505];
+const ll INF = 1LL*2500*2500*4000 + 10;
 
 int main() {
   ios::sync_with_stdio(0); cin.tie(0);
   int n, m; cin>>n>>m;
-  cout<<"hi"<<endl;
-  vector<int> c(n);
-  vector<vector<pii>> a(n);
+  vector<ll> c(n);
+  vector<vector<pil>> a(n);
   for (auto &i : c) cin>>i;
   for (int i = 0; i < m; i++) {
-    int u, v, w; cin>>u>>v>>w;
+    int u, v; ll w; cin>>u>>v>>w;
     a[u-1].push_back({v-1, w});
     a[v-1].push_back({u-1, w});
   }
@@ -33,7 +34,7 @@ int main() {
         pq.push({{d[v][uc], min(uc, c[v])}, v});
       }
   }
-  int ans = INF;
+  ll ans = INF;
   for (int i = 1; i <= 2500; i++) ans = min(ans, d[n-1][i]);
   cout<<ans<<"\n";
 }
