b234691b20567ab6c46a9bfeeaa26b6ba382fa4b
hajin-chung <hajinchung1@gmail.com>
2025-01-02 18:37:39 +0900

    clean

--- boj/5528.cpp
+++ boj/5528.cpp
@@ -16,13 +16,12 @@ int main() {
     b[m-i] = c == 'I';
   }
   dp[1][0] = a[1]; dp[0][1] = b[1];
-  for (int i = 1; i <= n; i++)
-    for (int j = 1; j <= m; j++) {
-      if (i == 1 && j == 1) continue;
+  for (int i = 0; i <= n; i++)
+    for (int j = 0; j <= m; j++) {
       int mx = a[i] || b[j];
       if (i > 1 && a[i] && !a[i-1] && dp[i-2][j]) mx = max(mx, dp[i-2][j]+2);
       if (j > 1 && b[j] && !b[j-1] && dp[i][j-2]) mx = max(mx, dp[i][j-2]+2);
-      if (((a[i] && !b[j]) || (!a[i] && b[j])) && dp[i-1][j-1]) 
+      if (i >= 1 && j >= 1 && ((a[i] && !b[j]) || (!a[i] && b[j])) && dp[i-1][j-1]) 
         mx = max(mx, dp[i-1][j-1]+2);
       dp[i][j] = mx;
     }
