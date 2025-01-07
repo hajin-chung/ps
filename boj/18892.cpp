06eddb98dedcf202d883305542b7e08715d18493
hajin-chung <hajinchung1@gmail.com>
2025-01-07 18:58:27 +0900

    sync

--- boj/18892.cpp
+++ boj/18892.cpp
@@ -1,27 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-typedef long long int ll;
 int n, k;
-vector<ll> a, depth, chk, ch[505], sz;
-vector<ll> ans;
+vector<int> a, depth, chk, ch[505], sz, idx;
+vector<int> ans;
 bool flag = false;
 
-ll f(int u) {
+int f(int u) {
   if (chk[u]) return depth[u]; 
   chk[u] = 1;
-  ll mx = 0;
-  for (int i = u+1; i <= n; i++)
-    if (a[i] > a[u]) {
-      ll vd = f(i);
+  int mx = 0;
+  for (int i = a[u]+1; i <= n; i++)
+    if (u < idx[i]) {
+      int v = idx[i];
+      int vd = f(v);
       if (mx == vd) {
-        ch[u].push_back(i);
-        sz[u] += sz[i];
+        ch[u].push_back(v);
+        sz[u] += sz[v];
       } else if (mx < vd) {
         mx = vd;
         ch[u].clear();
-        ch[u].push_back(i);
-        sz[u] = sz[i];
+        ch[u].push_back(v);
+        sz[u] = sz[v];
       }
     }
   if (sz[u] == 0) sz[u] = 1;
@@ -31,30 +31,19 @@ ll f(int u) {
 int main() {
   ios::sync_with_stdio(0); cin.tie(0);
   cin>>n>>k;
-  a.resize(n+1); depth.resize(n+1); chk.resize(n+1); sz.resize(n+1);
-  for (int i = 1; i <= n; i++) cin>>a[i];
-  f(0);
-  vector<bool> cc(n+1);
-  queue<int> q;
-  q.push(0);
-  while (!q.empty()) {
-    int u = q.front(); q.pop();
-    cc[u] = 1;
-    sort(ch[u].begin(), ch[u].end(), [&](int u, int v) { return a[u] < a[v]; });
-    for (auto v : ch[u])
-      if (!cc[v]) {
-        cc[v] = 1;
-        q.push(v);
-      }
+  a.resize(n+1); depth.resize(n+1); chk.resize(n+1); sz.resize(n+1), idx.resize(n+1);
+  for (int i = 1; i <= n; i++) {
+    cin>>a[i];
+    idx[a[i]] = i;
   }
-  for (int i = 0; i <= n; i++)
+  f(0);
   if (sz[0] < k) {
     cout<<"-1\n";
     return 0;
   }
   int u = 0;
   while (k && ch[u].size()) {
-    ll sum = 0;
+    int sum = 0;
     for (auto v : ch[u]) {
       if (sz[v] + sum >= k) {
         ans.push_back(v);
