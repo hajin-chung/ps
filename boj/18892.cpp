32a2a615a81f902cead7edac9dbbe2c6d9f9f4c1
hajin-chung <hajinchung1@gmail.com>
2025-01-07 18:32:15 +0900

    sync

---
+++ boj/18892.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, k;
+vector<int> a, depth, chk, ch[505], sz;
+vector<int> ans;
+bool flag = false;
+
+int f(int u) {
+  if (chk[u]) return depth[u]; 
+  chk[u] = 1;
+  int mx = 0;
+  for (int i = u+1; i <= n; i++)
+    if (a[i] > a[u]) {
+      int vd = f(i);
+      if (mx == vd) {
+        ch[u].push_back(i);
+        sz[u] += sz[i];
+      } else if (mx < vd) {
+        mx = vd;
+        ch[u].clear();
+        ch[u].push_back(i);
+        sz[u] = sz[i];
+      }
+    }
+  if (sz[u] == 0) sz[u] = 1;
+  return depth[u] = mx + 1;
+}
+
+int main() {
+  ios::sync_with_stdio(0); cin.tie(0);
+  cin>>n>>k;
+  a.resize(n+1); depth.resize(n+1); chk.resize(n+1); sz.resize(n+1);
+  for (int i = 1; i <= n; i++) cin>>a[i];
+  f(0);
+  for (int i = 0; i <= n; i++)
+    sort(ch[i].begin(), ch[i].end(), [&](int u, int v) { return a[u] < a[v]; });
+  int u = 0;
+  while (k && ch[u].size()) {
+    if (sz[u] < k) {
+      cout<<"-1\n";
+      return 0;
+    }
+    int sum = 0;
+    for (auto v : ch[u]) {
+      if (sz[v] + sum >= k) {
+        ans.push_back(v);
+        k -= sum;
+        u = v;
+        break;
+      }
+      sum += sz[v];
+    }
+  }
+  for (auto ai : ans) cout<<a[ai]<<" ";
+  cout<<"\n";
+}
