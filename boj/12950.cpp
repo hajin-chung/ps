6ff96bbb6ed4afd13dd13a3b5c2e8eb2b8ddd68c
hajin-chung <hajinchung1@gmail.com>
2025-01-08 18:32:36 +0900

    sync

--- boj/12950.cpp
+++ boj/12950.cpp
@@ -1,39 +1,40 @@
 #include <bits/stdc++.h>
-#define INF 1000000000
+#define pb push_back
 using namespace std;
 
-int n, m;
-set<int> a[20][26];
-int dp[20][20];
-
-int f(int u, int v) {
-  cout<<u<<" "<<v<<"\n";
-  if (dp[u][v] != INF) return dp[u][v];
-  if (u == v) return dp[u][v] = 0;
-  bool flag = 0;
-  for (int i = 0; i < 26; i++)
-    flag |= a[u][i].count(v);
-  if (flag) return dp[u][v] = 1;
-  int ret = INF;
-  for (int i = 0; i < 26; i++)
-    for (auto nu : a[u][i])
-      for (auto nv : a[v][i])
-        ret = min(ret, f(nu, nv)+2);
-  return dp[u][v] = ret;
-}
+const int INF = 1e9;
 
 int main() {
   ios::sync_with_stdio(0); cin.tie(0);
-  cin>>n>>m;
+  int n, m; cin>>n>>m;
+  vector<int> g[20][26];
   for (int i = 0; i < m; i++) {
     int u, v; char c; cin>>u>>v>>c;
-    a[u][c-'a'].insert(v);
-    a[v][c-'a'].insert(u);
+    g[u][c-'a'].pb(v);
+    g[v][c-'a'].pb(u);
   }
+  bool dp[105][20][20];
+  memset(dp, 0, sizeof(dp));
+  for (int i = 0; i < n; i++) dp[1][i][i] = 1;
   for (int i = 0; i < n; i++)
-    for (int j = 0; j < n; j++)
-      dp[i][j] = INF;
-  int ans = f(0, 1);
-  if (ans == INF) cout<<"-1\n";
-  else cout<<ans<<"\n";
+    for (int c = 0; c < 26; c++)
+      for (auto j : g[i][c])
+        dp[2][i][j] = 1;
+  for (int l = 3; l <= 100; l++) {
+    for (int i = 0; i < n; i++)
+      for (int j = 0; j < n; j++) {
+        if (i == j) continue;
+        for (int c = 0; c < 26; c++)
+          for (auto u : g[i][c])
+            for (auto v : g[j][c])
+              if (dp[l-2][u][v]) {
+                dp[l][i][j] = 1;
+              }
+        if (dp[l][0][1]) {
+          cout<<l-1<<"\n";
+          return 0;
+        }
+      }
+  }
+  cout<<"-1\n";
 }
