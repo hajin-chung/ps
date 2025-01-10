d6b9b61810b9af316356f7bcc490e7e11983e291
hajin-chung <hajinchung1@gmail.com>
2025-01-10 19:43:16 +0900

    correctly prasing query param

--- boj/15420.cpp
+++ boj/15420.cpp
@@ -50,7 +50,7 @@ vector<pll> get_hull(vector<pll> &a) {
 
 int main() {
   ios::sync_with_stdio(0); cin.tie(0);
-  int n, r; cin>>n>>r;
+  int n, r; cin>>n;
   vector<pll> a(n);
   for (auto &[x, y] : a) cin>>x>>y;
   auto hull = get_hull(a);
