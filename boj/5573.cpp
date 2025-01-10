97b24f78c73d768cb299d224d81ce859e533ab7d
hajin-chung <hajinchung1@gmail.com>
2025-01-10 19:10:10 +0900

    correctly prasing query param

--- boj/5573.cpp
+++ boj/5573.cpp
@@ -1,30 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-bool a[1005][1005];
-int dp[1005][1005];
-
 int main() {
   ios::sync_with_stdio(0); cin.tie(0);
   int n, m, t; cin>>n>>m>>t;
+  bool a[1005][1005];
+  int dp[1005][1005];
+  memset(dp, 0, sizeof(dp));
   for (int i = 0; i < n; i++)
     for (int j = 0; j < m; j++)
       cin>>a[i][j];
   dp[0][0] = t-1;
   for (int i = 0; i < n; i++)
     for (int j = 0; j < m; j++) {
-      dp[i+1][j] = dp[i][j]/2 + (dp[i][j] % 2 && !a[i][j]);
-      dp[i][j+1] = dp[i][j]/2 + (dp[i][j] % 2 && a[i][j]);
+      dp[i+1][j] += dp[i][j]/2 + (dp[i][j] % 2 && !a[i][j]);
+      dp[i][j+1] += dp[i][j]/2 + (dp[i][j] % 2 && a[i][j]);
     }
   int yy = 0, xx = 0;
   while (yy < n && xx < m) {
-    if (dp[yy][xx]%2) {
-      if (a[yy][xx]) yy++;
-      else xx++;
-    } else {
-      if (a[yy][xx]) xx++;
-      else yy++;
-    }
+    if (dp[yy][xx]%2 == a[yy][xx]) yy++;
+    else xx++;
   }
   cout<<yy+1<<" "<<xx+1<<"\n";
 }
