b3cd783b39af5a4f5efae87dfb8c7888bd17c9e3
hajin-chung <hajinchung1@gmail.com>
2025-01-12 13:42:23 +0900

    sync

--- boj/2220.cpp
+++ boj/2220.cpp
@@ -4,6 +4,17 @@ using namespace std;
 int main() {
   ios::sync_with_stdio(0); cin.tie(0);
   int n; cin>>n;
-  for (int i = n; i >= 1; i--) cout<<i<<" ";
+  vector<int> ans;
+  ans.push_back(1);
+  for (int i = 2; i <= n; i++) {
+    ans.push_back(i);
+    int u = i-1;
+    while (u) {
+      int v = u>>1;
+      if (ans[v] < ans[u]) swap(ans[v], ans[u]);
+      u = v;
+    }
+  }
+  for (auto ai : ans) cout<<ai<<" ";
   cout<<"\n";
 }
