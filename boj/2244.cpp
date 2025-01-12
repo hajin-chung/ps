245bce0b645b4e3f9a5763141ee034323b597fc2
hajin-chung <hajinchung1@gmail.com>
2025-01-12 16:29:49 +0900

    sync

--- boj/2244.cpp
+++ boj/2244.cpp
@@ -20,6 +20,7 @@ int ccw(pll a, pll b, pll c) {
 
 vector<pll> get_hull(vector<pll> &a) {
   int n = a.size();
+  a.erase(unique(a.begin(), a.end()), a.end());
   for (int i = 1; i < n; i++) if (a[i] < a[0]) swap(a[i], a[0]);
   sort(a.begin(), a.end(), [&](pll u, pll v) {
     int c = ccw(a[0], u, v);
