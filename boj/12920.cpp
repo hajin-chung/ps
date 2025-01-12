b2a8df3ed498e4ade7890593b7d06f46c6ed7490
hajin-chung <hajinchung1@gmail.com>
2025-01-12 09:05:33 +0900

    correctly prasing query param

--- boj/12920.cpp
+++ boj/12920.cpp
@@ -3,16 +3,17 @@ using namespace std;
 
 const int INF = 1e9;
 const int M = 10000;
+typedef long long int ll;
 
 int main() {
   ios::sync_with_stdio(0); cin.tie(0);
   int n, m; cin>>n>>m;
-  vector<int> dp(m+1, -1);
+  vector<ll> dp(m+1, -1);
   dp[0] = 0;
-  int ans = 0;
+  ll ans = 0;
   for (int i = 0; i < n; i++) {
     auto tmp = dp;
-    int v, c, k; cin>>v>>c>>k;
+    ll v, c, k; cin>>v>>c>>k;
     for (int ki = 1; ki <= k; ki++)
       for (int j = v*ki; j <= m; j++)
         if (dp[j-v*ki] != -1 && dp[j] < dp[j-v*ki]+c*ki) {
@@ -21,5 +22,7 @@ int main() {
         }
     dp = tmp;
   }
+  for (int i = 1; i <= m; i++) cout<<dp[i]<<" ";
+  cout<<"\n";
   cout<<ans<<"\n";
 }
