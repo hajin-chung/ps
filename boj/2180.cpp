dab648fdf2fea8bff5e2b61f2489743f434bd934
hajin-chung <hajinchung1@gmail.com>
2025-01-12 12:11:04 +0900

    sync

--- boj/2180.cpp
+++ boj/2180.cpp
@@ -16,10 +16,8 @@ int main() {
   sort(fire.begin(), fire.end(), [](pll &u, pll &v) {
     return v.fi*u.se < u.fi*v.se;
   });
-  ll ans = 0, t = 0;
-  for (int i = 0; i < n; i++) {
-    ans += fire[i].fi*ans+fire[i].se;
-    ans %= MOD;
-  }
-  cout<<ans<<"\n";
+  ll t = 0;
+  for (int i = 0; i < n; i++)
+    t += fire[i].fi*t+fire[i].se;
+  cout<<t%MOD<<"\n";
 }
