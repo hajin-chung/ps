fb075f61318ad34983a7f37faa85c991696fed69
hajin-chung <hajinchung1@gmail.com>
2025-01-17 20:38:46 +0900

    sync

--- boj/31397.cpp
+++ boj/31397.cpp
@@ -41,7 +41,7 @@ int main() {
   ld l = 0, r = c/2, tarea = area(a);
   ld st, et, a1, a2;
   int sidx, eidx;
-  int trial = 10000;
+  int trial = 10;
   while (trial--) {
     ld offset = (l+r)/2;
     sidx = upper_bound(d.begin(), d.end(), offset)-d.begin();
