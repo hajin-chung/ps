885f4a11a7d880f8935e0d16ee58ad67fbbd3ecb
hajin-chung <hajinchung1@gmail.com>
2025-01-19 14:48:07 +0900

    sync

--- boj/3090.cpp
+++ boj/3090.cpp
@@ -1,9 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+typedef long long int ll;
+
 int main() {
   ios::sync_with_stdio(0); cin.tie(0);
-  int n, t; cin>>n>>t;
+  int n; ll t; cin>>n>>t;
   vector<int> a(n);
   for (auto &i : a) cin>>i;
   int l = 0, r = 0;
@@ -11,17 +13,17 @@ int main() {
   vector<int> ta, ans;
   while (l < r) {
     int m = (l+r)>>1;
-    int cnt = 0;
+    ll cnt = 0;
     ta = a;
     for (int i = 1; i < n; i++) 
       if (ta[i]-ta[i-1] > m) {
         cnt += ta[i]-ta[i-1]-m;
         ta[i] = ta[i-1] + m;
       }
-    for (int i = n-2; i >= 0; i--) 
-      if (ta[i]-ta[i+1] > m) {
-        cnt += ta[i]-ta[i+1]-m;
-        ta[i] = ta[i+1] + m;
+    for (int i = n-1; i >= 1; i--) 
+      if (ta[i-1]-ta[i] > m) {
+        cnt += ta[i-1]-ta[i]-m;
+        ta[i-1] = ta[i] + m;
       }
     if (cnt > t) l = m+1;
     else {
