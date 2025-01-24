7113f235c5cdaf38b6c4e7c4ef883bb5b6bd2a9d
hajin-chung <hajinchung1@gmail.com>
2025-01-24 18:34:25 +0900

    sync

---
+++ boj/2123.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+#define fi first
+#define se second
+using namespace std;
+
+typedef long long int ll;
+typedef pair<ll, ll> pll;
+
+int main() {
+  ios::sync_with_stdio(0); cin.tie(0);
+  int n; cin>>n;
+  vector<pll> a(n);
+  ll sum = 0;
+  for (int i = 0; i < n; i++) {
+    ll w, s; cin>>w>>s;
+    a[i] = {w, s};
+    sum += w;
+  }
+  sort(a.begin(), a.end(), [](pll u, pll v) { return u.fi+u.se > v.fi+v.se; });
+  ll mx = 0;
+  for (auto [w, s] : a) {
+    mx = max(mx, sum-w-s);
+    sum -= w;
+  }
+  cout<<mx<<"\n";
+}
