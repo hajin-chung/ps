129f5e1b47abaae144a61b088918e72f1933a5c4
hajin-chung <hajinchung1@gmail.com>
2025-01-26 14:33:51 +0900

    sync

--- boj/25406.cpp
+++ boj/25406.cpp
@@ -4,38 +4,46 @@
 using namespace std;
 
 typedef pair<int, int> pii;
-const int INF = 1e9;
+const int N = 300000;
+int n, a[N+1], cnt[N+1];
+map<int, int> mp;
+set<pii> cset, iset;
 
 int main() {
   ios::sync_with_stdio(0); cin.tie(0);
-  int n; cin>>n;
-  vector<int> a(n+1);
-  map<int, int> mp;
-  set<pii> st;
-  int cur = 0;
+  cin>>n;
+  int k = 1;
   for (int i = 1; i <= n; i++) {
     int x; cin>>x;
-    if (!mp.count(x)) mp[x] = cur++;
+    if (!mp.count(x)) mp[x] = k++;
     a[i] = mp[x];
-    st.insert({a[i], i});
+    cnt[a[i]]++;
   }
-  vector<int> ans(n+1);
-  ans[1] = 1;
-  st.erase(st.begin());
-  for (int i = 2; i <= n; i++) {
-    cout<<ans[i-1]<<" ";
-    if (a[ans[i-1]] == st.begin()->fi) {
-      auto it = st.upper_bound({a[ans[i-1]], INF});
-      if (it == st.end()) {
-        cout<<"-1\n";
-        return 0;
-      }
-      ans[i] = it->se; st.erase(it);
-    } else {
-      ans[i] = st.begin()->se;
-      st.erase(st.begin());
+  for (int i = 1; i <= n; i++) {
+    if (cnt[a[i]])
+      cset.insert({cnt[a[i]], a[i]});
+    iset.insert({a[i], i});
+  }
+  if (2*cset.rbegin()->fi > n+1) {
+    cout<<"-1\n";
+    return 0;
+  }
+  int last = -1;
+  for (int i = n; i >= 1; i--) {
+    int k;
+    if (2*cset.rbegin()->fi == i+1) k = cset.rbegin()->se;
+    else {
+      k = iset.begin()->fi;
+      if (k == last) k = iset.upper_bound({k+1, -1})->fi;
     }
+    auto it = iset.lower_bound({k, -1});
+    cout<<it->se<<" ";
+    cset.erase({cnt[k], k});
+    cnt[k]--;
+    cset.insert({cnt[k], k});
+    iset.erase(it);
+    last = k;
   }
-  for (auto ai : ans) cout<<ai<<" ";
   cout<<"\n";
+  return 0;
 }
