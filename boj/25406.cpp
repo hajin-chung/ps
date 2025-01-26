fa1510a5ed1f523cf31a79caaae2d1ce6f1880d4
hajin-chung <hajinchung1@gmail.com>
2025-01-26 15:23:27 +0900

    sync

--- boj/25406.cpp
+++ boj/25406.cpp
@@ -6,24 +6,24 @@ using namespace std;
 typedef pair<int, int> pii;
 const int N = 300000;
 int n, a[N+1], cnt[N+1];
-map<int, int> mp;
-set<pii> cset, iset;
+set<int> idx[N+1];
 
 int main() {
   ios::sync_with_stdio(0); cin.tie(0);
   cin>>n;
   int k = 1;
   for (int i = 1; i <= n; i++) {
-    int x; cin>>x;
-    if (!mp.count(x)) mp[x] = k++;
-    a[i] = mp[x];
+    cin>>a[i];
     cnt[a[i]]++;
+    idx[a[i]].insert(i);
   }
-  for (int i = 1; i <= n; i++) {
-    if (cnt[i])
+  set<pii> cset;
+  set<pii> iset;
+  for (int i = 1; i <= n; i++)
+    if (cnt[i]) {
       cset.insert({cnt[i], i});
-    iset.insert({a[i], i});
-  }
+      iset.insert({*idx[i].begin(), i});
+    }
   if (2*cset.rbegin()->fi > n+1) {
     cout<<"-1\n";
     return 0;
@@ -33,15 +33,17 @@ int main() {
     int k;
     if (2*cset.rbegin()->fi == i+1) k = cset.rbegin()->se;
     else {
-      k = iset.begin()->fi;
-      if (k == last) k = iset.upper_bound({k+1, -1})->fi;
+      auto it = iset.begin();
+      if (it->se == last) it++;
+      k = it->se;
     }
-    auto it = iset.lower_bound({k, -1});
-    cout<<it->se<<" ";
+    cout<<*idx[k].begin()<<" ";
     cset.erase({cnt[k], k});
     cnt[k]--;
     cset.insert({cnt[k], k});
-    iset.erase(it);
+    iset.erase({*idx[k].begin(), k});
+    idx[k].erase(idx[k].begin());
+    if (idx[k].size()) iset.insert({*idx[k].begin(), k});
     last = k;
   }
   cout<<"\n";
