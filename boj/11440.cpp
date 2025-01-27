9dc6fd286b42bbfddc9650ee7da803411a163ee1
hajin-chung <hajinchung1@gmail.com>
2025-01-27 15:53:54 +0900

    sync

---
+++ boj/11440.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long int ll;
+typedef tuple<ll,ll,ll,ll> t4i;
+const ll MOD = 1e9+7;
+
+t4i mult(t4i &a, t4i &b) {
+  auto [a11, a21, a12, a22] = a;
+  auto [b11, b21, b12, b22] = b;
+  return {
+    (a11*b11+a12*b21)%MOD,
+    (a21*b11+a22*b21)%MOD,
+    (a11*b12+a12*b22)%MOD,
+    (a21*b12+a22*b22)%MOD,
+  };
+}
+
+t4i pow(t4i a, ll n) {
+  t4i res = {1, 0, 0, 1};
+  while (n>0) {
+    if (n&1) res = mult(res, a);
+    res = mult(res, res);
+    n>>=1;
+  }
+  return res;
+}
+
+int main() {
+  ios::sync_with_stdio(0); cin.tie(0);
+  ll n; cin>>n;
+  t4i a = {1, 1, 1, 0};
+  auto [fn, fm, x, y] = pow(a, n);
+  cout<<fn*fm%MOD<<"\n";
+}
