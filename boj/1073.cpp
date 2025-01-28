e0dbcb73e211bc1578498a4c1d270fe1e01de9d6
hajin-chung <hajinchung1@gmail.com>
2025-01-28 16:09:23 +0900

    sync

---
+++ boj/1073.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long int ll;
+int cnt[10], d[10];
+
+int main() {
+  ios::sync_with_stdio(0); cin.tie(0);
+  int n; cin>>n;
+  while (n--) {
+    int x; cin>>x;
+    cnt[x/10]++; cnt[x%10]++;
+  }
+  d[0] = 1;
+  for (int i = 2; i <= 8; i++) d[i] = (i-1)*d[i-2];
+  ll ans = 1;
+  for (int i = 1; i < 10; i++) ans *= d[cnt[i]];
+  cout<<ans<<"\n";
+}
