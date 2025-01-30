6d9c476a09c9eb419a328f7369830f254e65e285
hajin-chung <hajinchung1@gmail.com>
2025-01-30 12:33:15 +0900

    sync

--- boj/13709.cpp
+++ boj/13709.cpp
@@ -11,16 +11,15 @@ int main() {
   for (auto &x : a) cin>>x;
   ll ans = 0;
   for (int i = 60; i >= 0; i--) {
-    ll mx = 0; int idx = -1;
+    ll mx = -1;
     for (int j = 0; j < n; j++)
-      if ((a[j]&(1ll<<i)) && a[j] > mx) {
-        mx = a[j]; idx = j;
-      }
-    if (idx == -1) continue;
-    ans ^= a[idx];
+      if ((a[j]&(1ll<<i)) && a[j] > mx)
+        mx = a[j];
+    if (mx == -1) continue;
+    ans ^= mx;
     for (auto &x : a)
       if (x&(1ll<<i))
-        x ^= a[idx];
+        x ^= mx;
   }
   cout<<ans<<"\n";
 }
