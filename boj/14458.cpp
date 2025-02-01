043b5bc63be9150c03568a88dd3e536fffa26fbf
hajin-chung <hajinchung1@gmail.com>
2025-02-01 13:39:23 +0900

    sync

--- boj/14458.cpp
+++ boj/14458.cpp
@@ -5,41 +5,30 @@ typedef long long int ll;
 const int N = 100000;
 const ll INF = 1e12;
 ll bit[N+1];
+int a[2][N+1];
 int n;
-
-void update(int i, ll v) {
-  for (; i <= n; i += i&-i) bit[i]+=v;
-}
-
-ll query(int i) {
-  int ret = 0;
-  for (; i; i -= i&-i) ret += bit[i];
-  return ret;
-}
 
 int main() {
   ios::sync_with_stdio(0); cin.tie(0);
   cin>>n;
-  vector<int> a(n+1), b(n+1);
-  for (int i = 1; i <= n; i++) cin>>a[i];
-  for (int i = 1; i <= n; i++) cin>>b[i];
+  for (int i = 1; i <= n; i++) cin>>a[0][i];
+  for (int i = 1; i <= n; i++) cin>>a[1][i];
   ll ans = INF;
   vector<int> idx(n+1);
-  for (int i = 1; i <= n; i++) idx[b[i]] = i;
+  for (int i = 1; i <= n; i++) idx[a[1][i]] = i;
   ll icnt = 0;
   for (int i = n; i >= 1; i--) {
-    icnt += query(idx[a[i]]);
-    update(idx[a[i]], 1);
+    for (int j = idx[a[0][i]]; j; j -= j&-j) icnt += bit[j];
+    for (int j = idx[a[0][i]]; j <= n; j += j&-j) bit[j]++;
   }
   for (int it = 0; it < 2; it++) {
     ll cnt = icnt;
     ans = min(ans, cnt);
-    for (int i = 1; i <= n; i++) idx[b[i]] = i;
+    for (int i = 1; i <= n; i++) idx[a[1-it][i]] = i;
     for (int i = 1; i <= n; i++) {
-      cnt = cnt - (idx[a[i]]-1) + (n-idx[a[i]]);
+      cnt = cnt - (idx[a[it][i]]-1) + (n-idx[a[it][i]]);
       ans = min(ans, cnt);
     }
-    swap(a, b);
   }
   cout<<ans<<"\n";
 }
