56a588e6c3ee35d3824baa8be6371b26dda51d6a
hajin-chung <hajinchung1@gmail.com>
2025-02-01 13:16:52 +0900

    sync

--- boj/14458.cpp
+++ boj/14458.cpp
@@ -4,48 +4,40 @@ using namespace std;
 typedef long long int ll;
 const int N = 100000;
 const ll INF = 1e12;
-ll seg[4*N+5];
+ll bit[N+1];
+int n;
 
-void update(int node, int l, int r, int i) {
-  if (i < l || r < i) return; 
-  if (l == r && r == i) {
-    seg[node] = 1;
-    return;
-  }
-  int m = (l+r)>>1;
-  update(node*2, l, m, i);
-  update(node*2+1, m+1, r, i);
-  seg[node] = seg[node*2]+seg[node*2+1];
+void update(int i) {
+  for (; i <= n; i += i&-i) bit[i]++;
 }
 
-ll query(int node, int l, int r, int i) {
-  if (r < i) return 0;
-  if (i <= l) return seg[node];
-  int m = (l+r)>>1;
-  ll rl = query(node*2, l, m, i);
-  ll rr = query(node*2+1, m+1, r, i);
-  return rl + rr;
+ll query(int i) {
+  int ret = 0;
+  for (; i; i -= i&-i) ret += bit[i];
+  return ret;
 }
 
 int main() {
   ios::sync_with_stdio(0); cin.tie(0);
-  int n; cin>>n;
-  vector<int> a(n+1), b(n+1), c(n+1), idx(n+1);
+  cin>>n;
+  vector<int> a(n+1), b(n+1), c(n+1), to(n+1), idx(n+1);
   for (int i = 1; i <= n; i++) cin>>a[i];
   for (int i = 1; i <= n; i++) cin>>b[i];
   ll ans = INF;
   for (int it = 0; it < 2; it++) {
+    c = b;
     for (int i = 1; i <= n; i++) idx[a[i]] = i;
-    for (int i = 1; i <= n; i++) c[idx[i]] = b[i];
-    for (int i = 0; i < 4*n+5; i++) seg[i] = 0;
+    for (int i = 1; i <= n; i++) c[i] = idx[c[i]];
+    for (int i = 1; i <= n; i++) to[c[i]] = i;
+    for (int i = 1; i <= n; i++) bit[i] = 0;
     ll cnt = 0;
-    for (int i = 1; i <= n; i++) {
-      update(1, 1, n, c[i]);
-      if (c[i] != n) cnt += query(1, 1, n, c[i]+1);
+    for (int i = n; i >= 1; i--) {
+      cnt += query(to[i]);
+      update(to[i]);
     }
     ans = min(ans, cnt);
-    for (int i = 1; i <= n; i++) {
-      cnt = cnt-(c[i]-1)+(n-c[i]);
+    for (int i = n; i >= 1; i--) {
+      cnt += c[i]*2-n+1;
       ans = min(ans, cnt);
     }
     swap(a, b);
