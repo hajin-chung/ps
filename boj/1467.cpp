501731f9b170c0fae9ab711f4eb28c506162262c
hajin-chung <hajinchung1@gmail.com>
2025-02-01 15:02:00 +0900

    sync

--- boj/1467.cpp
+++ boj/1467.cpp
@@ -4,9 +4,10 @@ using namespace std;
 int main() {
   string s, t; cin>>s>>t;
   int n = s.size();
+  sort(t.begin(), t.end());
   vector<bool> alive(n, 1);
   for (auto c : t) {
-    bool flag = 0; int idx = -1, i = 0;
+    bool flag = 0; int idx = -1, i = 0, aaa;
     if (!alive[i]) while (!alive[i] && i<n) i++;
     while (i < n) {
       int k = i++;
@@ -15,6 +16,7 @@ int main() {
         idx = k;
         if (i == n) break;
         if (s[i] > s[k]) {
+          aaa = k;
           alive[k] = 0;
           flag = 1;
           break;
@@ -22,6 +24,7 @@ int main() {
       }
     }
     if (!flag) {
+      aaa = idx;
       alive[idx] = 0;
     }
   }
