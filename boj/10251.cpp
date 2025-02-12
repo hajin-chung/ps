d7593bbb9e4ef807b568009b7a2b1299fb871fd7
hajin-chung <hajinchung1@gmail.com>
2025-02-12 18:30:33 +0900

    sync

---
+++ boj/10251.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int INF = 1e9;
+const int N = 100;
+const int M = 100;
+int a[N][M][2];
+int dp[N][M][N+M][2];
+
+void mn(int &x, int v) { if (x > v) x = v; }
+
+void f() {
+  int n, m, g, l; cin>>n>>m>>l>>g;
+  int mxd = 2*(min(n, m)-1)-1;
+  for (int i = 0; i < n; i++)
+    for (int j = 0; j < m-1; j++)
+      cin>>a[i][j][0];
+  for (int i = 0; i < n-1; i++)
+    for (int j = 0; j < m; j++)
+      cin>>a[i][j][1];
+  for (int i = 0; i < n; i++)
+    for (int j = 0; j < m; j++)
+      for (int d = 0; d <= mxd; d++)
+        dp[i][j][d][0] = dp[i][j][d][1] = INF;
+  dp[0][0][0][0] = dp[0][0][0][1] = 0;
+  for (int i = 0; i < n; i++)
+    for (int j = 0; j < m; j++)
+      for (int d = 0; d <= mxd; d++) {
+        if (i) {
+          mn(dp[i][j][d][1], dp[i-1][j][d][1]+a[i-1][j][1]);
+          if (d) mn(dp[i][j][d][1], dp[i-1][j][d-1][0]+a[i-1][j][1]);
+        }
+        if (j) {
+          mn(dp[i][j][d][0], dp[i][j-1][d][0]+a[i][j-1][0]);
+          if (d) mn(dp[i][j][d][0], dp[i][j-1][d-1][1]+a[i][j-1][0]);
+        }
+      }
+  int ans = -1;
+  for (int d = 0; d <= mxd; d++)
+    if (dp[n-1][m-1][d][1] <= g || dp[n-1][m-1][d][0] <= g) {
+      ans = d;
+      break;
+    }
+  if (ans != -1) {
+    cout<<(n+m-2)*l+ans<<"\n";
+  } else cout<<ans<<"\n";
+}
+
+int main() {
+  ios::sync_with_stdio(0); cin.tie(0);
+  int t; cin>>t;
+  while (t--) f();
+}
--- boj/1657.cpp
+++ boj/1657.cpp
@@ -3,7 +3,14 @@
 using namespace std;
 
 const int N = 14;
-int s[5][5] = {{10, 8, 7, 5, 1}, {8, 6, 4, 3, 1}, {7, 4, 3, 2, 1}, {5, 3, 2, 2, 1}, {1, 1, 1, 1, 0}};
+int s[6][6] = {
+  {10, 8, 7, 5, 0, 1}, 
+  {8, 6, 4, 3, 0, 1}, 
+  {7, 4, 3, 2, 0, 1}, 
+  {5, 3, 2, 2, 0, 1}, 
+  {0, 0, 0, 0, 0, 0},
+  {1, 1, 1, 1, 0, 0}
+};
 int a[N][N];
 int dp[2][1<<N];
 
@@ -17,6 +24,10 @@ int main() {
       char c; cin>>c;
       a[i][j] = c-'A';
     }
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < m; j++) cout<<a[i][j]<<" ";
+    cout<<"\n";
+  }
   int ans = 0;
   for (int i = 0; i < n; i++) {
     int k = i&1;
@@ -24,18 +35,20 @@ int main() {
     for (int bit = 0; bit < (1<<m); bit++) {
       for (int j = 0; j < m; j++) {
         if (bit & (1<<j)) 
-          mx(dp[k][bit], dp[k][bit&~(1<<j)]);
-        if (j > 0 && (bit & ((1<<j)|(1<<(j-1))))) {
-          int b = bit&~((1<<j)|(1<<(j-1)));
-          mx(dp[k][bit], dp[k][b]+s[a[i][j]][a[i][j-1]]);
+          mx(dp[k][bit], dp[k][bit&~(1<<j)]+dp[1-k][(1<<m)-1]);
+        if (j > 0 && (bit & (1<<j)) && (bit & (1<<(j-1)))) {
+          int b = (bit&~(1<<j))&~(1<<(j-1));
+          mx(dp[k][bit], dp[k][b]+s[a[i][j]][a[i][j-1]]+dp[1-k][(1<<m)-1]);
         }
         if (i > 0 && (bit & (1<<j))) {
           int b2 = ((1<<m)-1)&~(1<<j), b1 = bit&~(1<<j);
           mx(dp[k][bit], dp[k][b1]+dp[1-k][b2]+s[a[i][j]][a[i-1][j]]);
         }
       }
+      if (ans < dp[k][bit]) cout<<dp[k][bit]<<" "<<bit<<"\n";
       mx(ans, dp[k][bit]);
     }
+      cout<<ans<<"\n";
   }
   cout<<ans<<"\n";
 }
